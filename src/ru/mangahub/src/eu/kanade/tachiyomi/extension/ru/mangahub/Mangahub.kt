b6fde1d2918226d4ba54716f66a6be17cb81bb8c
JustRoxy <roxygamesrus@mail.ru>
2018-09-22 23:47:08 +0300

    Delete comments

--- src/ru/mangahub/src/eu/kanade/tachiyomi/extension/ru/mangahub/Mangahub.kt
+++ src/ru/mangahub/src/eu/kanade/tachiyomi/extension/ru/mangahub/Mangahub.kt
@@ -46,13 +46,6 @@ open class Mangahub : ParsedHttpSource() {
     override fun latestUpdatesNextPageSelector() = ".next"
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-//        val url = HttpUrl.parse("$baseUrl/search/advanced")!!.newBuilder()
-//
-//        if (!query.isEmpty()) {
-//            url.addQueryParameter("q", query)
-//        }
-//        return GET(url.toString().replace("=%3D", "="), headers)
-
         return GET("$baseUrl/search/manga?query=$query&page=${page + 1}")
     }
 
@@ -60,7 +53,6 @@ open class Mangahub : ParsedHttpSource() {
 
     override fun searchMangaFromElement(element: Element): SManga = popularMangaFromElement(element)
 
-    // max 200 results
     override fun searchMangaNextPageSelector(): Nothing? = null
 
     override fun mangaDetailsParse(document: Document): SManga {
@@ -105,30 +97,6 @@ open class Mangahub : ParsedHttpSource() {
         }
     }
 
-//    override fun pageListParse(response: Response): List<Page> {
-//        val html = response.body()!!.string()
-//        val beginIndex = html.indexOf("rm_h.init( [")
-//        val endIndex = html.indexOf("], 0, false);", beginIndex)
-//        val trimmedHtml = html.substring(beginIndex, endIndex)
-//
-//        val p = Pattern.compile("'.*?','.*?',\".*?\"")
-//        val m = p.matcher(trimmedHtml)
-//
-//        val pages = mutableListOf<Page>()
-//
-//        var i = 0
-//        while (m.find()) {
-//            val urlParts = m.group().replace("[\"\']+".toRegex(), "").split(',')
-//            val url = if (urlParts[1].isEmpty() && urlParts[2].startsWith("/static/")) {
-//                baseUrl + urlParts[2]
-//            } else {
-//                urlParts[1] + urlParts[0] + urlParts[2]
-//            }
-//            pages.add(Page(i++, "", url))
-//        }
-//        return pages
-//    }
-
     override fun pageListParse(document: Document): List<Page> {
         val pictures = document.select("div.b-reader.b-reader__full").attr("data-js-scans").replace("&quot;", "\"").replace("\\/", "/")
         val r = Regex("""\/\/([\w\.\/])+""")
@@ -151,78 +119,4 @@ open class Mangahub : ParsedHttpSource() {
         }.build()
         return GET(page.imageUrl!!, imgHeader)
     }
-
-// private class Genre(name: String, val id: String) : Filter.TriState(name)
-// private class GenreList(genres: List<Genre>) : Filter.Group<Genre>("Genres", genres)
-// private class Category(categories: List<Genre>) : Filter.Group<Genre>("Category", categories)
-
-    /* [...document.querySelectorAll("tr.advanced_option:nth-child(1) > td:nth-child(3) span.js-link")]
-    *  .map(el => `Genre("${el.textContent.trim()}", "${el.getAttribute('onclick')
-    *  .substr(31,el.getAttribute('onclick').length-33)"})`).join(',\n')
-    *  on http://mintmanga.com/search/advanced
-    */
-//    override fun getFilterList() = FilterList(
-//            Category(getCategoryList()),
-//            GenreList(getGenreList())
-//    )
-
-//    private fun getCategoryList() = listOf(
-//            Genre("В цвете", "el_4614"),
-//            Genre("Веб", "el_1355"),
-//            Genre("Выпуск приостановлен", "el_5232"),
-//            Genre("Ёнкома", "el_2741"),
-//            Genre("Комикс западный", "el_1903"),
-//            Genre("Комикс русский", "el_2173"),
-//            Genre("Манхва", "el_1873"),
-//            Genre("Маньхуа", "el_1875"),
-//            Genre("Не Яой", "el_1874"),
-//            Genre("Ранобэ", "el_5688"),
-//            Genre("Сборник", "el_1348")
-//    )
-//
-//    private fun getGenreList() = listOf(
-//            Genre("арт", "el_2220"),
-//            Genre("бара", "el_1353"),
-//            Genre("боевик", "el_1346"),
-//            Genre("боевые искусства", "el_1334"),
-//            Genre("вампиры", "el_1339"),
-//            Genre("гарем", "el_1333"),
-//            Genre("гендерная интрига", "el_1347"),
-//            Genre("героическое фэнтези", "el_1337"),
-//            Genre("детектив", "el_1343"),
-//            Genre("дзёсэй", "el_1349"),
-//            Genre("додзинси", "el_1332"),
-//            Genre("драма", "el_1310"),
-//            Genre("игра", "el_5229"),
-//            Genre("история", "el_1311"),
-//            Genre("киберпанк", "el_1351"),
-//            Genre("комедия", "el_1328"),
-//            Genre("меха", "el_1318"),
-//            Genre("мистика", "el_1324"),
-//            Genre("научная фантастика", "el_1325"),
-//            Genre("омегаверс", "el_5676"),
-//            Genre("повседневность", "el_1327"),
-//            Genre("постапокалиптика", "el_1342"),
-//            Genre("приключения", "el_1322"),
-//            Genre("психология", "el_1335"),
-//            Genre("романтика", "el_1313"),
-//            Genre("самурайский боевик", "el_1316"),
-//            Genre("сверхъестественное", "el_1350"),
-//            Genre("сёдзё", "el_1314"),
-//            Genre("сёдзё-ай", "el_1320"),
-//            Genre("сёнэн", "el_1326"),
-//            Genre("сёнэн-ай", "el_1330"),
-//            Genre("спорт", "el_1321"),
-//            Genre("сэйнэн", "el_1329"),
-//            Genre("трагедия", "el_1344"),
-//            Genre("триллер", "el_1341"),
-//            Genre("ужасы", "el_1317"),
-//            Genre("фантастика", "el_1331"),
-//            Genre("фэнтези", "el_1323"),
-//            Genre("школа", "el_1319"),
-//            Genre("эротика", "el_1340"),
-//            Genre("этти", "el_1354"),
-//            Genre("юри", "el_1315"),
-//            Genre("яой", "el_1336")
-//    )
 }
