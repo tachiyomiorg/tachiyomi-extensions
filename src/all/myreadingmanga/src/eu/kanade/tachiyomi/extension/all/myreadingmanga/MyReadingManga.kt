a5c3d87dbe0308fe1ccadd9b96f07dc2202f674e
mll-Kerberos <mll.kerberos@gmail.com>
2018-10-21 12:28:39 +0200

    Changed the Page parser for MyReadingManga, index was always the same

--- src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingManga.kt
+++ src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingManga.kt
@@ -165,7 +165,7 @@ open class MyReadingManga(override val lang: String) : ParsedHttpSource() {
         val pages = mutableListOf<Page>()
         val elements = body.select(".separator [data-lazyloaded='1']")
         for (i in 0 until elements.size) {
-            pages.add(Page(1, "", getImage(elements[i])))
+            pages.add(Page(i, "", getImage(elements[i])))
         }
         return pages
     }
