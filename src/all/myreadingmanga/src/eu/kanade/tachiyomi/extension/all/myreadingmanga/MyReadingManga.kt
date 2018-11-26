fb835b3cad44ac0335e6ad0a70c20b902da62d7b
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2018-11-26 01:32:22 -0500

    Update MyReadingManga.kt
    Fixed a jpeg tag with the thumbnail, duplicate data-src changed to just src

--- src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingManga.kt
+++ src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingManga.kt
@@ -92,7 +92,7 @@ open class MyReadingManga(override val lang: String) : ParsedHttpSource() {
         var url =
                 when {
                     element.attr("data-src").endsWith(".jpg") || element.attr("data-src").endsWith(".png") || element.attr("data-src").endsWith(".jpeg") -> element.attr("data-src")
-                    element.attr("src").endsWith(".jpg") || element.attr("src").endsWith(".png") || element.attr("data-src").endsWith(".jpeg") -> element.attr("src")
+                    element.attr("src").endsWith(".jpg") || element.attr("src").endsWith(".png") || element.attr("src").endsWith(".jpeg") -> element.attr("src")
                     else -> element.attr("data-lazy-src")
                 }
         if (url.startsWith("//")) {
