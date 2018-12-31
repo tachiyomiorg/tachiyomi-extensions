fe00bb27dade0f7cc2f8a9aa983ef6ce3d9a291c
Eugene Cheung <ey.cheung@outlook.com>
2018-12-31 18:11:31 -0500

    Fix Mangafox (fixes #620)

--- src/en/mangafox/src/eu/kanade/tachiyomi/extension/en/mangafox/Mangafox.kt
+++ src/en/mangafox/src/eu/kanade/tachiyomi/extension/en/mangafox/Mangafox.kt
@@ -1,4 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.mangafox
+import android.util.Log
+import com.squareup.duktape.Duktape
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.*
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
@@ -9,6 +11,7 @@ import org.jsoup.nodes.Element
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.regex.Pattern
 
 class Mangafox : ParsedHttpSource() {
 
@@ -22,14 +25,14 @@ class Mangafox : ParsedHttpSource() {
 
     override val supportsLatest = true
 
-    override fun popularMangaSelector() = "div#mangalist > ul.list > li"
+    override fun popularMangaSelector() = ".manga-list-1 > ul > li"
 
     override fun popularMangaRequest(page: Int): Request {
         val pageStr = if (page != 1) "$page.htm" else ""
         return GET("$baseUrl/directory/$pageStr", headers)
     }
 
-    override fun latestUpdatesSelector() = "div#mangalist > ul.list > li"
+    override fun latestUpdatesSelector() = popularMangaSelector()
 
     override fun latestUpdatesRequest(page: Int): Request {
         val pageStr = if (page != 1) "$page.htm" else ""
@@ -38,72 +41,63 @@ class Mangafox : ParsedHttpSource() {
 
     override fun popularMangaFromElement(element: Element): SManga {
         val manga = SManga.create()
-        element.select("a.title").first().let {
+        element.select("a").first().let {
             manga.setUrlWithoutDomain(it.attr("href"))
-            manga.title = it.text()
+            manga.title = it.attr("title")
+            manga.thumbnail_url = it.select("img").first().attr("src")
         }
         return manga
     }
 
-    override fun latestUpdatesFromElement(element: Element): SManga {
-        return popularMangaFromElement(element)
-    }
+    override fun latestUpdatesFromElement(element: Element) = popularMangaFromElement(element)
 
-    override fun popularMangaNextPageSelector() = "a:has(span.next)"
+    override fun popularMangaNextPageSelector() = ".pager-list-left a.active + a + a"
 
-    override fun latestUpdatesNextPageSelector() = "a:has(span.next)"
+    override fun latestUpdatesNextPageSelector() = popularMangaNextPageSelector()
 
+    //    http://fanfox.net/search?title=&genres=&st=0&sort=&stype=1&name_method=cw&name=&author_method=cw&author=&artist_method=cw&artist=&type=&rating_method=eq&rating=&released_method=eq&released=
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val url = HttpUrl.parse("$baseUrl/search.php?name_method=cw&author_method=cw&artist_method=cw&advopts=1")!!.newBuilder().addQueryParameter("name", query)
-        (if (filters.isEmpty()) getFilterList() else filters).forEach { filter ->
-            when (filter) {
-                is Status -> url.addQueryParameter(filter.id, filter.state.toString())
-                is GenreList -> filter.state.forEach { genre -> url.addQueryParameter(genre.id, genre.state.toString()) }
-                is TextField -> url.addQueryParameter(filter.key, filter.state)
-                is Type -> url.addQueryParameter("type", if (filter.state == 0) "" else filter.state.toString())
-                is OrderBy -> {
-                    url.addQueryParameter("sort", arrayOf("name", "rating", "views", "total_chapters", "last_chapter_time")[filter.state!!.index])
-                    url.addQueryParameter("order", if (filter.state?.ascending == true) "az" else "za")
-                }
-            }
-        }
+        val url = HttpUrl.parse("$baseUrl/search")!!.newBuilder()
+
+        // Basic title search
+        url.addQueryParameter("title", query)
+
+        // More search filters
+//        (if (filters.isEmpty()) getFilterList() else filters).forEach { filter ->
+//            when (filter) {
+//                is Status -> url.addQueryParameter(filter.id, filter.state.toString())
+//                is GenreList -> filter.state.forEach { genre -> url.addQueryParameter(genre.id, genre.state.toString()) }
+//                is TextField -> url.addQueryParameter(filter.key, filter.state)
+//                is Type -> url.addQueryParameter("type", if (filter.state == 0) "" else filter.state.toString())
+//                is OrderBy -> {
+//                    url.addQueryParameter("sort", arrayOf("name", "rating", "views", "total_chapters", "last_chapter_time")[filter.state!!.index])
+//                    url.addQueryParameter("order", if (filter.state?.ascending == true) "az" else "za")
+//                }
+//            }
+//        }
+
+        // Search results page
         url.addQueryParameter("page", page.toString())
-        return GET(url.toString(), headers)
+
+        return GET(url.build().url().toString(), headers)
     }
 
-    override fun searchMangaSelector() = "div#mangalist > ul.list > li"
+    override fun searchMangaSelector() = ".manga-list-4 > ul > li"
 
-    override fun searchMangaFromElement(element: Element): SManga {
-        val manga = SManga.create()
-        element.select("a.title").first().let {
-            manga.setUrlWithoutDomain(it.attr("href"))
-            manga.title = it.text()
-        }
-        return manga
-    }
+    override fun searchMangaFromElement(element: Element) = popularMangaFromElement(element)
 
-    override fun searchMangaNextPageSelector() = "a:has(span.next)"
+    override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
 
     override fun mangaDetailsParse(document: Document): SManga {
-        val infoElement = document.select("div#title").first()
-        val rowElement = infoElement.select("table > tbody > tr:eq(1)").first()
-        val sideInfoElement = document.select("#series_info").first()
-        val licensedElement = document.select("div.warning").first()
+        val infoElement = document.select(".detail-info-right").first()
 
-        val manga = SManga.create()
-        manga.author = rowElement.select("td:eq(1)").first()?.text()
-        manga.artist = rowElement.select("td:eq(2)").first()?.text()
-        manga.genre = rowElement.select("td:eq(3)").first()?.text()
-        manga.description = infoElement.select("p.summary").first()?.text()
-        val isLicensed = licensedElement?.text()?.contains("licensed")
-        if (isLicensed == true) {
-            manga.status = SManga.LICENSED
-        } else {
-            manga.status = sideInfoElement.select(".data").first()?.text().orEmpty().let { parseStatus(it) }
+        return SManga.create().apply {
+            author = infoElement.select(".detail-info-right-say a").joinToString(", ") { it.text() }
+            genre = infoElement.select(".detail-info-right-tag-list a").joinToString(", ") { it.text() }
+            description = infoElement.select("p.fullcontent").first()?.text()
+            status = infoElement.select(".detail-info-right-title-tip").first()?.text().orEmpty().let { parseStatus(it) }
+            thumbnail_url = document.select(".detail-info-cover-img").first()?.attr("src")
         }
-
-        manga.thumbnail_url = sideInfoElement.select("div.cover > img").first()?.attr("src")
-        return manga
     }
 
     private fun parseStatus(status: String) = when {
@@ -112,16 +106,14 @@ class Mangafox : ParsedHttpSource() {
         else -> SManga.UNKNOWN
     }
 
-    override fun chapterListSelector() = "div#chapters li div"
+    override fun chapterListSelector() = "ul.detail-main-list li a"
 
     override fun chapterFromElement(element: Element): SChapter {
-        val urlElement = element.select("a.tips").first()
-
-        val chapter = SChapter.create()
-        chapter.setUrlWithoutDomain(urlElement.attr("href"))
-        chapter.name = element.select("span.title.nowrap").first()?.text()?.let { urlElement.text() + " - " + it } ?: urlElement.text()
-        chapter.date_upload = element.select("span.date").first()?.text()?.let { parseChapterDate(it) } ?: 0
-        return chapter
+        return SChapter.create().apply {
+            setUrlWithoutDomain(element.attr("href"))
+            name = element.select(".detail-main-list-main p").first()?.text().orEmpty()
+            date_upload = element.select(".detail-main-list-main p").last()?.text()?.let { parseChapterDate(it) } ?: 0
+        }
     }
 
     private fun parseChapterDate(date: String): Long {
@@ -142,7 +134,7 @@ class Mangafox : ParsedHttpSource() {
             }.timeInMillis
         } else {
             try {
-                SimpleDateFormat("MMM d, yyyy", Locale.ENGLISH).parse(date).time
+                SimpleDateFormat("MMM d,yyyy", Locale.ENGLISH).parse(date).time
             } catch (e: ParseException) {
                 0L
             }
@@ -152,24 +144,109 @@ class Mangafox : ParsedHttpSource() {
     override fun pageListParse(document: Document): List<Page> {
         val url = document.baseUri().substringBeforeLast('/')
 
+        val totalPages = document.select(".pager-list-left > span > a:nth-last-child(2)").first().text().toInt()
+
         val pages = mutableListOf<Page>()
-        document.select("select.m").first()?.select("option:not([value=0])")?.forEach {
-            pages.add(Page(pages.size, "$url/${it.attr("value")}.html"))
+        for (i in 1..totalPages) {
+            pages.add(Page(i, "$url/$i.html"))
         }
         return pages
     }
 
     override fun imageUrlParse(document: Document): String {
-        val url = document.getElementById("image").attr("src")
-        return if ("compressed?token=" !in url) {
-            url
-        } else {
-            "http://mangafox.me/media/logo.png"
+        // Eval the JS code, which either returns an array of image URLs, or the mkey
+        val packedJS = document.getElementsByTag("script").map {
+            it.data().trim()
+        }.find {
+            it.startsWith(PACKED_JS_BEGIN_MARKER) && it.endsWith(PACKED_JS_END_MARKER)
+        }
+
+        val unpackedJS = unpackJS(packedJS)
+
+        var mkey = ""
+
+        // Case where JS returns something like:
+        //
+        //   var guidkey=''+'8'+'f'+'4'+'1'+'d'+'1'+'f'+'1'+'a'+'d'+'9'+'3'+'4'+'7'+'e'+'e';
+        //   $("#dm5_key").val(guidkey);
+        if (unpackedJS?.contains("guidkey")!!) {
+            mkey = Duktape.create().use {
+                // Remove jQuery line that we don't need and just return the evaluated guidkey
+                val script = unpackedJS.replace("\$(\"#dm5_key\").val(guidkey);", "") +
+                        GUIDKEY_JS
+
+                it.evaluate(script) as String
+            }
+        }
+
+        // TODO: handle case where unpackedJS contains an array of img URLs directly
+
+        val docHtml = document.body().html()
+
+        var chapterid = ""
+        var m = chapteridPattern.matcher(docHtml)
+        while (m.find()) {
+            chapterid = m.group(1)
+        }
+
+        var imagepage = ""
+        m = imagepagePattern.matcher(docHtml)
+        while (m.find()) {
+            imagepage = m.group(1)
+        }
+
+        // TODO: below is still a WIP
+
+        // Evaluate JS code returned from GET call, which returns an array of image URLs
+        val res = getImage(document, chapterid, imagepage, mkey)
+
+        Log.d("Mangafox", "res: $res")
+
+        if (res.contains(PACKED_JS_BEGIN_MARKER)) {
+            val stuff = unpackJS(res)
+
+//            val urlString = result.split(", ")[1]
+
+            Log.d("Mangafox", "stuff: $stuff")
+        }
+
+        return ""
+    }
+
+    private fun getImage(document: Document, chapterId: String, imagePage: String, mkey: String): String {
+        val uri = "${document.location().replaceAfterLast("/", "")}chapterfun.ashx"
+
+        val url = HttpUrl.parse(uri)!!.newBuilder()
+        url.addQueryParameter("cid", chapterId)
+        url.addQueryParameter("page", imagePage)
+        url.addQueryParameter("key", mkey)
+
+        Log.d("Mangafox", "url: $url")
+
+        return client.newCall(GET(url.build().url().toString(), headers)).execute().body()!!.string()
+    }
+
+    private fun unpackJS(code: String?): String? {
+        if (code == null) {
+            return null
+        }
+
+        return Duktape.create().use {
+            // We need to preserve backslashes and quotes when the string is parsed
+            val escapedJS = code
+                    .replace("\\", "\\\\")
+                    .replace("\"", "\\\"")
+
+            val script = """var PACKED_JS = "$escapedJS";""" +
+                    UNPACKER_JS_SRC +
+                    UNPACKER_JS
+
+            it.evaluate(script) as String
         }
     }
 
     private class Status(val id: String = "is_completed") : Filter.TriState("Completed")
-    private class Genre(name: String, val id: String = "genres[$name]") : Filter.TriState(name)
+    private class Genre(val id: String, name: String) : Filter.TriState(name)
     private class TextField(name: String, val key: String) : Filter.Text(name)
     private class Type : Filter.Select<String>("Type", arrayOf("Any", "Japanese Manga", "Korean Manhwa", "Chinese Manhua"))
     private class OrderBy : Filter.Sort("Order by",
@@ -187,44 +264,126 @@ class Mangafox : ParsedHttpSource() {
             GenreList(getGenreList())
     )
 
-    // $('select.genres').map((i,el)=>`Genre("${$(el).next().text().trim()}", "${$(el).attr('name')}")`).get().join(',\n')
-    // on http://mangafox.me/search.php
+    // [...document.querySelectorAll('.tagbt a')].sort((a, b) => a.innerText.localeCompare(b.innerText)).map(n => `Genre("${n.dataset.val}", "${n.innerText}")`).join(',\n')
+    // on http://fanfox.net/search
     private fun getGenreList() = listOf(
-            Genre("Action"),
-            Genre("Adult"),
-            Genre("Adventure"),
-            Genre("Comedy"),
-            Genre("Doujinshi"),
-            Genre("Drama"),
-            Genre("Ecchi"),
-            Genre("Fantasy"),
-            Genre("Gender Bender"),
-            Genre("Harem"),
-            Genre("Historical"),
-            Genre("Horror"),
-            Genre("Josei"),
-            Genre("Martial Arts"),
-            Genre("Mature"),
-            Genre("Mecha"),
-            Genre("Mystery"),
-            Genre("One Shot"),
-            Genre("Psychological"),
-            Genre("Romance"),
-            Genre("School Life"),
-            Genre("Sci-fi"),
-            Genre("Seinen"),
-            Genre("Shoujo"),
-            Genre("Shoujo Ai"),
-            Genre("Shounen"),
-            Genre("Shounen Ai"),
-            Genre("Slice of Life"),
-            Genre("Smut"),
-            Genre("Sports"),
-            Genre("Supernatural"),
-            Genre("Tragedy"),
-            Genre("Webtoons"),
-            Genre("Yaoi"),
-            Genre("Yuri")
+            Genre("1", "Action"),
+            Genre("33", "Adult"),
+            Genre("2", "Adventure"),
+            Genre("3", "Comedy"),
+            Genre("25", "Doujinshi"),
+            Genre("4", "Drama"),
+            Genre("19", "Ecchi"),
+            Genre("5", "Fantasy"),
+            Genre("32", "Gender Bender"),
+            Genre("10", "Harem"),
+            Genre("30", "Historical"),
+            Genre("8", "Horror"),
+            Genre("29", "Josei"),
+            Genre("6", "Martial Arts"),
+            Genre("22", "Mature"),
+            Genre("35", "Mecha"),
+            Genre("15", "Mystery"),
+            Genre("26", "One Shot"),
+            Genre("11", "Psychological"),
+            Genre("12", "Romance"),
+            Genre("13", "School Life"),
+            Genre("16", "Sci-fi"),
+            Genre("17", "Seinen"),
+            Genre("14", "Shoujo"),
+            Genre("23", "Shoujo Ai"),
+            Genre("7", "Shounen"),
+            Genre("31", "Shounen Ai"),
+            Genre("21", "Slice of Life"),
+            Genre("27", "Smut"),
+            Genre("20", "Sports"),
+            Genre("9", "Supernatural"),
+            Genre("18", "Tragedy"),
+            Genre("24", "Webtoons"),
+            Genre("28", "Yaoi"),
+            Genre("34", "Yuri")
     )
+
+    companion object {
+        val chapteridPattern by lazy {
+            Pattern.compile("var chapterid =(\\d+);")!!
+        }
+
+        val imagepagePattern by lazy {
+            Pattern.compile("var imagepage=(\\d+);")!!
+        }
+
+        const val PACKED_JS_BEGIN_MARKER = "eval(function(p,a,c,k,e,d){"
+        const val PACKED_JS_END_MARKER = "}))"
+
+        const val UNPACKER_JS_SRC = """
+            //////////////////////////////////////////
+            //  Un pack the code from the /packer/  //
+            //  By matthew@matthewfl.com            //
+            //  http://matthewfl.com/unPacker.html  //
+            //////////////////////////////////////////
+            // version 1.2
+
+
+            function unPack (code) {
+                function indent (code) {
+                    try {
+                    var tabs = 0, old=-1, add='';
+                    for(var i=0;i<code.length;i++) {
+                        if(code[i].indexOf("{") != -1) tabs++;
+                        if(code[i].indexOf("}") != -1) tabs--;
+
+                        if(old != tabs) {
+                            old = tabs;
+                            add = "";
+                            while (old > 0) {
+                                add += "\t";
+                                old--;
+                            }
+                            old = tabs;
+                        }
+
+                        code[i] = add + code[i];
+                    }
+                    } finally {
+                        tabs = null;
+                        old = null;
+                        add = null;
+                    }
+                    return code;
+                }
+
+                var env = {
+                    eval: function (c) {
+                        code = c;
+                    },
+                    window: {},
+                    document: {}
+                };
+
+                eval("with(env) {" + code + "}");
+
+                code = (code+"").replace(/;/g, ";\n").replace(/{/g, "\n{\n").replace(/}/g, "\n}\n").replace(/\n;\n/g, ";\n").replace(/\n\n/g, "\n");
+
+                code = code.split("\n");
+                code = indent(code);
+
+                code = code.join("\n");
+                return code;
+            }
+        """
+
+        const val UNPACKER_JS = """
+            (function() {
+                return unPack(PACKED_JS);
+            })();
+        """
+
+        const val GUIDKEY_JS = """
+            (function() {
+                return guidkey;
+            })();
+        """
+    }
 
 }
