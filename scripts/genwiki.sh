d7ad6e5a90789eeca312545b87cee4d5317a0c1b
phanirithvij <phanirithvij20003@gmail.com>
2019-02-24 19:55:43 +0530

    replaced md syntax with bold and italic tags

--- scripts/genwiki.sh
+++ scripts/genwiki.sh
@@ -12,47 +12,47 @@ TreeURL="https://github.com/inorichi/tachiyomi-extensions/tree/master/src"
 BlobURL="https://github.com/inorichi/tachiyomi-extensions/blob/master"
 
 lang_from_langcode(){
-   case "$1" in
+    case "$1" in
     "all")
-      echo "All Languages"
-      ;;
+        echo "All Languages"
+    ;;
     "en")
-      echo "English"
-      ;;
+        echo "English"
+    ;;
     "vi")
-      echo "Vietnamese (Tiếng Việt)"
-      ;;
+        echo "Vietnamese (Tiếng Việt)"
+    ;;
     "ru")
-      echo "Russian (Pусский)"
-      ;;
+        echo "Russian (Pусский)"
+    ;;
     "it")
-      echo "Italian (Italiano)"
-      ;;
+        echo "Italian (Italiano)"
+    ;;
     "es")
-      echo "Spanish (Español)"
-      ;;
+        echo "Spanish (Español)"
+    ;;
     "ko")
-      echo "Korean (한국어)"
-      ;;
+        echo "Korean (한국어)"
+    ;;
     "ja")
-      echo "Japanese (日本語)"
-      ;;
+        echo "Japanese (日本語)"
+    ;;
     "pt")
-      echo "Portuguese (Português)"
-      ;;
+        echo "Portuguese (Português)"
+    ;;
     "fr")
-      echo "French (Français)"
-      ;;
+        echo "French (Français)"
+    ;;
     "de")
-      echo "German (Deutsch)"
-      ;;
+        echo "German (Deutsch)"
+    ;;
     "zh")
-      echo "Chinese (中文)"
-      ;;
+        echo "Chinese (中文)"
+    ;;
     *)
-      echo "NONE"
-      ;;
-   esac
+        echo "NONE"
+    ;;
+    esac
 }
 
 echo -e "# Extensions\n" >> Extensions.md
@@ -62,49 +62,52 @@ echo -e "**Page under construction**" >> Extensions.md
 
 for langcode in $(ls src/)
 do 
-   lang=$(lang_from_langcode $langcode)
-   echo Language $lang
-   echo -e "\n## $lang\n" >> Extensions.md
-  for shrtname in $(ls src/$langcode/)
-  do
+    lang=$(lang_from_langcode $langcode)
+    echo Language $lang
+    echo -e "\n## $lang\n" >> Extensions.md
+    echo -e "<table border=\"0\">" >> Extensions.md
+    for shrtname in $(ls src/$langcode/)
+    do
         # Language
-      echo short name $shrtname
-         # Title
-      name=$(grep 'appName' src/$langcode/$shrtname/build.gradle | cut -d '=' -f2 | awk '{split($0,a,"Tachiyomi:"); print a[2]}')
-      name=${name::-1}
-      name=${name:1}
-      echo Name is $name
-         # Version
-      code=$(grep 'extVersionCode' src/$langcode/$shrtname/build.gradle | cut -d '=' -f2)
-      version=$(grep 'libVersion' src/$langcode/$shrtname/build.gradle | cut -d '=' -f2)
-      version=${version::-1}
-      version=${version:2}
-      version=$version.${code:1}
-      echo Version $version
-   #   cat src/$langcode/$shrtname/build.gradle
-      # Image
-      image=src/$langcode/$shrtname/res/web_hi_res_512.png
-      if [ -f $image ]; then
-         echo Image Exists
-      else
-         echo Using Default Image
-         image="res/mipmap-xxxhdpi/ic_launcher.png"
-      fi
-      
-      # echo -e "**${name:1}** *$version*" >> Extensions.md
-      echo -e "<table border=\"0\">" >> Extensions.md
-      echo -e "<tr>" >> Extensions.md
-      echo -e "<td rowspan=\"2\">" >> Extensions.md
-      echo -e "<a href=\"$TreeURL/$langcode/$shrtname\">" >> Extensions.md
-      echo -e "<img src=\"$BlobURL/$image?raw=true\" width=\"60\" alt=\"$name\">" >> Extensions.md
-      echo -e "</a>" >> Extensions.md
-      echo -e "<img/>" "</td>" >> Extensions.md
-      echo -e "<td> " >> Extensions.md
-      echo -e "<a href=\"$TreeURL/$langcode/$shrtname\">" >> Extensions.md
-      echo -e "\n\n**${name}**\n\n </td>" >> Extensions.md
-      echo -e "</a>" >> Extensions.md
-      echo -e "</tr>" "<tr>" >> Extensions.md
-      echo -e "<td> \n\n*"${version}"*\n\n </td>" >> Extensions.md
-      echo -e "</tr>" "</table>" >> Extensions.md
-  done
+        echo short name $shrtname
+        # Title
+        name=$(grep 'appName' src/$langcode/$shrtname/build.gradle | cut -d '=' -f2 | awk '{split($0,a,"Tachiyomi:"); print a[2]}')
+        name=${name::-1}
+        name=${name:1}
+        echo Name is $name
+        # Version
+        code=$(grep 'extVersionCode' src/$langcode/$shrtname/build.gradle | cut -d '=' -f2)
+        version=$(grep 'libVersion' src/$langcode/$shrtname/build.gradle | cut -d '=' -f2)
+        version=${version::-1}
+        version=${version:2}
+        version=$version.${code:1}
+        echo Version $version
+        # cat src/$langcode/$shrtname/build.gradle
+        # Image
+        image=src/$langcode/$shrtname/res/web_hi_res_512.png
+        if [ -f $image ]; then
+            echo Image Exists
+        else
+            echo Using Default Image
+            image="res/mipmap-xxxhdpi/ic_launcher.png"
+        fi
+
+        # echo -e "**${name:1}** *$version*" >> Extensions.md
+        echo -e "\t<tr>" >> Extensions.md
+        echo -e "\t\t<td rowspan=\"2\">" >> Extensions.md
+        echo -e "\t\t\t<a href=\"$TreeURL/$langcode/$shrtname\">" >> Extensions.md
+        echo -e "\t\t\t\t<img src=\"$BlobURL/$image?raw=true\" width=\"60\" alt=\"$name\"></img>" >> Extensions.md
+        echo -e "\t\t\t</a>" >> Extensions.md
+        echo -e "\t\t</td>" >> Extensions.md
+        echo -e "\t\t<td>" >> Extensions.md
+        echo -e "\t\t\t<a href=\"$TreeURL/$langcode/$shrtname\"><b>${name}</b></a>" >> Extensions.md
+        echo -e "\t\t</td>" >> Extensions.md
+        echo -e "\t</tr>" >> Extensions.md
+        echo -e "\t<tr>" >> Extensions.md
+        echo -e "\t\t<td>" >> Extensions.md
+        echo -e "\t\t\t<i>"${version}"</i>" >> Extensions.md
+        echo -e "\t\t</td>" >> Extensions.md
+        echo -e "\t</tr>" >> Extensions.md
+    done
+    echo -e "</table>" >> Extensions.md
 done
