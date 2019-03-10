df3e7b468abeee55cb84e16e536738c4d0bdc758
Carlos Escobedo <cargo8005@gmail.com>
2019-03-10 18:17:22 -0400

    fix ehentai build issues

--- lib/main-stub/src/main/java/android/net/Uri.java
+++ lib/main-stub/src/main/java/android/net/Uri.java
@@ -1,3 +1,17 @@
 package android.net;
 
-public class Uri {}
+public class Uri {
+
+    public static class Builder{
+        public Builder appendQueryParameter(String key, String value){
+            return new Builder();
+        }
+    }
+    public static Uri parse(String str){
+            return new Uri();
+    }
+    public  Builder buildUpon(){
+        return new Builder();
+    }
+
+}
