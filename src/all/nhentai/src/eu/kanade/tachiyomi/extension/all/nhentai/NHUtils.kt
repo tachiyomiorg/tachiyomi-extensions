f67b721e0d4f6baf50ac25eeaa5f637588df039f
az4521 <cheeze.sprinkels@gmail.com>
2019-03-21 20:38:08 -0400

    asdaSTLTLKTLsnglnsaltjkn

--- src/all/nhentai/src/eu/kanade/tachiyomi/extension/all/nhentai/NHUtils.kt
+++ src/all/nhentai/src/eu/kanade/tachiyomi/extension/all/nhentai/NHUtils.kt
@@ -1,52 +1,37 @@
 package eu.kanade.tachiyomi.extension.all.nhentai
 
 import org.jsoup.nodes.Document
-import java.lang.StringBuilder
 import java.text.SimpleDateFormat
 
 class NHUtils {
     companion object {
         fun getArtists(document: Document): String {
-            val stringBuilder = StringBuilder()
-            val artists = document.select("#tags > div:nth-child(4) > span > a")
-
-            artists.forEach {
-                stringBuilder.append(it.text().replace(Regex("\\(.*\\)"), ""))
-
-                if (it != artists.last())
-                    stringBuilder.append(", ")
-            }
-
-            return stringBuilder.toString()
+            return document.select("#tags > div:nth-child(4) > span > a")
+                .map { cleanTag(it.text()) }
+                .joinToString(", ") { it.trim() } 
         }
 
         fun getGroups(document: Document): String? {
-            val stringBuilder = StringBuilder()
             val groups = document.select("#tags > div:nth-child(5) > span > a")
+                .map { cleanTag(it.text()) }
+                .joinToString(", ") { it.trim() }
 
-            groups.forEach {
-                stringBuilder.append(it.text().replace(Regex("\\(.*\\)"), ""))
-
-                if (it != groups.last())
-                    stringBuilder.append(", ")
-            }
-
-            return if (stringBuilder.toString().isEmpty()) null else stringBuilder.toString()
+            return if (groups.isEmpty()) null else groups
         }
 
         fun getDesc(document: Document): String {
             val parodies = document.select("#tags > div:nth-child(1) > span > a")
-                .map { it.text().replace(Regex("\\(.*\\)"), "") }
+                .map { cleanTag(it.text()) }
                 .joinToString(", ") { it.trim() }
             val characters = document.select("#tags > div:nth-child(2) > span > a")
-                .map { it.text().replace(Regex("\\(.*\\)"), "") }
+                .map { cleanTag(it.text()) }
                 .joinToString(", ") { it.trim() }
             return parodies.plus("\n\n").plus(characters)
         }
 
         fun getTags(document: Document): String {
             return document.select("#tags > div:nth-child(3) > span > a")
-                .map { it.text().replace(Regex("\\(.*\\)"), "") }
+                .map { cleanTag(it.text()) }
                 .joinToString(", ") { it.trim() }            
         }
         
@@ -55,5 +40,6 @@ class NHUtils {
 
             return SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSSZ").parse(timeString).time
         }
+        private fun cleanTag(tag: String): String = tag.replace(Regex("\\(.*\\)"), "").trim()
     }
 }
