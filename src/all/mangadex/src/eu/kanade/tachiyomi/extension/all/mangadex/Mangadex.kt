f4c37836dc042191a829eb13943820cbb900f146
Zane Hannan <zanehannanau@gmail.com>
2019-05-23 14:07:01 +1000

    undo

--- src/all/mangadex/src/eu/kanade/tachiyomi/extension/all/mangadex/Mangadex.kt
+++ src/all/mangadex/src/eu/kanade/tachiyomi/extension/all/mangadex/Mangadex.kt
@@ -182,7 +182,7 @@ open class Mangadex(override val lang: String, private val internalLang: String,
                         val mangaId = json["manga_id"] as String
                         client.newCall(searchMangaByIdRequest(mangaId))
                             .asObservableSuccess()
-                            .flatMap { response -> 
+                            .map { response -> 
                                 val details = mangaDetailsParse(response)
                                 details.url = "/manga/$mangaId"
                                 MangasPage(listOf(details), false)
