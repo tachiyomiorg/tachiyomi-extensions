5251ebd73cfbd4f61b805e0aab91306d0f9df852
Alessandro Jean <alessandrojean@gmail.com>
2019-05-28 23:27:45 -0300

    Remove Log import from debug.

--- src/en/mangaplus/src/eu/kanade/tachiyomi/extension/en/mangaplus/MangaPlus.kt
+++ src/en/mangaplus/src/eu/kanade/tachiyomi/extension/en/mangaplus/MangaPlus.kt
@@ -1,6 +1,5 @@
 package eu.kanade.tachiyomi.extension.en.mangaplus
 
-import android.util.Log
 import com.github.salomonbrys.kotson.*
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
