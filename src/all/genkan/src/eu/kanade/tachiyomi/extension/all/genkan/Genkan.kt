fbcb15346c39fad1d77c8af4e775526625c549f1
snakedoc83 <mikepeterson83@gmail.com>
2019-07-05 12:48:37 -0400

    Moved to all

--- src/all/genkan/src/eu/kanade/tachiyomi/extension/all/genkan/Genkan.kt
+++ src/all/genkan/src/eu/kanade/tachiyomi/extension/all/genkan/Genkan.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.en.genkan
+package eu.kanade.tachiyomi.extension.all.genkan
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.*
--- src/all/genkan/src/eu/kanade/tachiyomi/extension/all/genkan/GenkanFactory.kt
+++ src/all/genkan/src/eu/kanade/tachiyomi/extension/all/genkan/GenkanFactory.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.en.genkan
+package eu.kanade.tachiyomi.extension.all.genkan
 
 import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.SourceFactory
