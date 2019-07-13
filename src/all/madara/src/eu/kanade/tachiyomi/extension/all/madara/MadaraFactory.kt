fbcf07fe05481be933176ada4f45d7e81733e7b0
unknown <Vesnyx@users.noreply.github.com>
2019-07-12 19:14:52 -0700

    Madara: Misc fixes and more sites added

--- src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
+++ src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
@@ -6,8 +6,13 @@ import eu.kanade.tachiyomi.source.SourceFactory
 import java.text.SimpleDateFormat
 import java.util.*
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.CacheControl
+import okhttp3.FormBody
 import okhttp3.Request
+import org.jsoup.nodes.Element
 
 class MadaraFactory : SourceFactory {
     override fun createSources(): List<Source> = listOf(
@@ -28,11 +33,15 @@ class MadaraFactory : SourceFactory {
         ZManga(),
         MangazukiMe(),
         MangazukiOnline(),
-        MangazukiClub(),
+        MangazukiClubJP(),
+        MangazukiClubKO(),
         FirstKissManga(),
         Mangalike(),
         MangaSY(),
-        ManwhaClub()
+        ManwhaClub(),
+        WuxiaWorld(),
+        YoManga(),
+        ManyToon()
     )
 }
 
@@ -58,10 +67,20 @@ class TritiniaScans : Madara("Tritinia Scans", "http://tritiniascans.ml/", "en")
 class TsubakiNoScan : Madara("Tsubaki No Scan", "https://tsubakinoscan.com/", "fr", dateFormat = SimpleDateFormat("dd/MM/yy", Locale.US))
 class YokaiJump : Madara("Yokai Jump", "https://yokaijump.fr/", "fr", dateFormat = SimpleDateFormat("dd/MM/yy", Locale.US))
 class ZManga : Madara("ZManga", "https://zmanga.org/", "es")
-class MangazukiMe : Madara("MangazukiMe", "https://mangazuki.me/", "en")
-class MangazukiOnline : Madara("MangazukiOnline", "https://www.mangazuki.online/", "en")
-class MangazukiClub : Madara("MangazukiClub", "https://mangazuki.club/", "ko")
+class MangazukiMe : Madara("Mangazuki.me", "https://mangazuki.me/", "en")
+class MangazukiOnline : Madara("Mangazuki.online", "https://www.mangazuki.online/", "en")
+class MangazukiClubJP : Madara("Mangazuki.club", "https://mangazuki.club/", "ja")
+class MangazukiClubKO : Madara("Mangazuki.club", "https://mangazuki.club/", "ko")
 class FirstKissManga : Madara("1st Kiss", "https://1stkissmanga.com/", "en")
 class Mangalike : Madara("Mangalike", "https://mangalike.net/", "en")
 class MangaSY : Madara("Manga SY", "https://www.mangasy.com/", "en")
 class ManwhaClub : Madara("Manwha Club", "https://manhwa.club/", "en")
+class WuxiaWorld : Madara("WuxiaWorld", "https://wuxiaworld.site/", "en") {
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/tag/webcomics/page/$page/?m_orderby=views", headers)
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/tag/webcomics/page/$page/?m_orderby=latest", headers)
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request = GET("$baseUrl/?s=$query&post_type=wp-manga&genre[]=webcomics", headers)
+    override fun latestUpdatesNextPageSelector(): String? = "div.nav-previous"
+    override fun popularMangaNextPageSelector(): String? = "div.nav-previous"
+}
+class YoManga : Madara("Yo Manga", "https://yomanga.info/", "en")
+class ManyToon : Madara("ManyToon", "https://manytoon.com/", "en")
