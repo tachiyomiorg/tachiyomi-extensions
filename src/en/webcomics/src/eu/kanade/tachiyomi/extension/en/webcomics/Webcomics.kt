0030d592fae299d2ecae6cdeeaadacce6b26ae0d
snakedoc83 <mikepeterson83@gmail.com>
2019-07-17 04:43:36 -0400

    Webcomics: Limit to 20 chapters

--- src/en/webcomics/src/eu/kanade/tachiyomi/extension/en/webcomics/Webcomics.kt
+++ src/en/webcomics/src/eu/kanade/tachiyomi/extension/en/webcomics/Webcomics.kt
@@ -56,6 +56,9 @@ class Webcomics : ParsedHttpSource() {
         manga.genre = infoElement.select(".labels > label").joinToString(", ") { it.text() }
         manga.description = infoElement.select("p.p-description").text()
         manga.thumbnail_url = infoElement.select("img").first()?.attr("src")
+        infoElement.select("p.p-schedule:first-of-type").text().let {
+            if (it.contains("IDK")) manga.status = SManga.COMPLETED else manga.status = SManga.ONGOING
+        }
         return manga
     }
 
@@ -112,7 +115,19 @@ class Webcomics : ParsedHttpSource() {
 
     override fun chapterListParse(response: Response): List<SChapter> {
         val document = response.asJsoup()
-        return document.select(chapterListSelector()).asReversed().map {chapterFromElement(it)}
+
+        /* Source only allows 20 chapters to be readable on their website, trying to read past
+           that results in a page list empty error; so might as well not grab them. */
+        if (document.select("${chapterListSelector()}:nth-child(21)").isEmpty()) {
+            return document.select(chapterListSelector()).asReversed().map { chapterFromElement(it) }
+        } else {
+            val chapters = mutableListOf<SChapter>()
+            for (i in 1..20)
+            document.select("${chapterListSelector()}:nth-child($i)").map{
+                chapters.add(chapterFromElement(it))
+            }
+            return chapters.reversed()
+        }
     }
 
     override fun chapterFromElement(element: Element): SChapter {
