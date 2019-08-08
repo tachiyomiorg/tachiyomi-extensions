1a3363bc1851eebfcdfefc4879dd0485f46afa15
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2019-08-08 01:54:23 -0400

    Adds chapter names
    Now parses chapter names from website if available.

--- src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingManga.kt
+++ src/all/myreadingmanga/src/eu/kanade/tachiyomi/extension/all/myreadingmanga/MyReadingManga.kt
@@ -132,18 +132,22 @@ open class MyReadingManga(override val lang: String) : ParsedHttpSource() {
 
     override fun chapterListSelector() = ".entry-pagination a"
 
+
     override fun chapterListParse(response: Response): List<SChapter> {
         val document = response.asJsoup()
         val chapters = mutableListOf<SChapter>()
 
         val date = parseDate(document.select(".entry-time").attr("datetime").substringBefore("T"))
+        val ch1name = document.select(".chapter-class a")?.first()?.text()?.capitalize() ?:"Chapter 1"
         //create first chapter since its on main manga page
-        chapters.add(createChapter("1", document.baseUri(), date))
+        chapters.add(createChapter("1", document.baseUri(), date, ch1name))
         //see if there are multiple chapters or not
         document.select(chapterListSelector())?.let { it ->
             it.forEach {
                 if (!it.text().contains("Next »", true)) {
-                    chapters.add(createChapter(it.text(), document.baseUri(), date))
+                    val pageNumber = it.text()
+                    val chname = document.select(".chapter-class a[href$=/$pageNumber/]")?.text()?.capitalize()
+                    chapters.add(createChapter(it.text(), document.baseUri(), date, chname))
                 }
             }
         }
@@ -156,10 +160,10 @@ open class MyReadingManga(override val lang: String) : ParsedHttpSource() {
         return SimpleDateFormat("yyyy-MM-dd").parse(date).time
     }
 
-    private fun createChapter(pageNumber: String, mangaUrl: String, date: Long): SChapter {
+    private fun createChapter(pageNumber: String, mangaUrl: String, date: Long, chname: String?): SChapter {
         val chapter = SChapter.create()
         chapter.setUrlWithoutDomain("$mangaUrl/$pageNumber")
-        chapter.name = "Ch. $pageNumber"
+        chapter.name = chname ?:"Chapter $pageNumber"
         chapter.date_upload = date
         return chapter
     }
