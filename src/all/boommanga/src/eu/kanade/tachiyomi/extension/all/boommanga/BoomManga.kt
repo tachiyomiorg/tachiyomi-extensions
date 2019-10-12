9f82aad35112be06533b66fc14327f696d8237e3
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2019-10-12 04:34:14 -0400

    Added ZH

--- src/all/boommanga/src/eu/kanade/tachiyomi/extension/all/boommanga/BoomManga.kt
+++ src/all/boommanga/src/eu/kanade/tachiyomi/extension/all/boommanga/BoomManga.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.en.boommanga
+package eu.kanade.tachiyomi.extension.all.boommanga
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.*
