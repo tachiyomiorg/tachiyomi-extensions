b5523f970f2008c2172b662cacf85c8b86adb3d6
Pavan Kumar Sampara <23577468+pavan3999@users.noreply.github.com>
2019-10-12 06:17:59 +0530

    wuxiaworld

--- src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
+++ src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
@@ -148,9 +148,9 @@ class MangaSY : Madara("Manga SY", "https://www.mangasy.com/", "en") {
 }
 class ManwhaClub : Madara("Manwha Club", "https://manhwa.club/", "en")
 class WuxiaWorld : Madara("WuxiaWorld", "https://wuxiaworld.site/", "en") {
-    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/tag/comics/page/$page/?m_orderby=views", headers)
-    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/tag/comics/page/$page/?m_orderby=latest", headers)
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList) = super.searchMangaRequest(page, "$query comics", filters)
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/tag/webcomic/page/$page/?m_orderby=views", headers)
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/tag/webcomic/page/$page/?m_orderby=latest", headers)
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList) = super.searchMangaRequest(page, "$query webcomic", filters)
 }
 class YoManga : Madara("Yo Manga", "https://yomanga.info/", "en") {
     override fun searchMangaNextPageSelector() = "nav.navigation-ajax"
