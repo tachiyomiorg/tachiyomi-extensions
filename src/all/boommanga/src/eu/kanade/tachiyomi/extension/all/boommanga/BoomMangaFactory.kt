7dac00d16ee529098c3d320fe0181404d0a15b5d
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2019-10-12 04:35:08 -0400

    Added ZH

--- src/all/boommanga/src/eu/kanade/tachiyomi/extension/all/boommanga/BoomMangaFactory.kt
+++ src/all/boommanga/src/eu/kanade/tachiyomi/extension/all/boommanga/BoomMangaFactory.kt
@@ -1,18 +1,8 @@
 package eu.kanade.tachiyomi.extension.en.boommanga
 
-import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.SourceFactory
-import eu.kanade.tachiyomi.source.model.FilterList
-import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
-import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Headers
-import java.text.SimpleDateFormat
-import java.util.Locale
-import okhttp3.Response
-import okhttp3.Request
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
@@ -21,11 +11,15 @@ class BoomMangaFactory : SourceFactory {
     override fun createSources(): List<Source> = listOf(
         BoomMangacom(),
         ManManga(),
-        TwinsComics()
+        TwinsComics(),
+        BoomMangazh(),
+        ManMangazh(),
+        TwinsComicszh()
     )
 }
 
 class BoomMangacom : BoomManga("BoomManga", "https://m.boommanga.com", "en")
+
 class ManManga: BoomManga("ManManga", "https://m.manmanga.com", "en"){
     override fun nameselector(element: Element) = element.select("a").attr("alt")
     override fun authorget(document: Document) = document.select(".author").text().substringAfter("：").trim()
@@ -49,6 +43,36 @@ class TwinsComics: BoomManga("TwinsComics", "https://m.twinscomics.com", "en"){
     override fun statusget(document: Document) = when (document.select(".type").text().substringAfter("：").trim()) {
         "Ongoing" -> SManga.ONGOING
         "Completed" -> SManga.COMPLETED
+        else -> SManga.UNKNOWN
+    }
+}
+
+class BoomMangazh : BoomManga("BoomManga", "https://m.boommanga.com/cn", "zh")
+
+class ManMangazh: BoomManga("ManManga", "https://m.manmanga.com/cn", "en"){
+    override fun nameselector(element: Element) = element.select("a").attr("alt")
+    override fun authorget(document: Document) = document.select(".author").text().substringAfter("：").trim()
+    override fun thumbnailget(document: Document) = document.select(".bg-box .bg").attr("style").substringAfter("'").substringBefore("'")
+    override fun genreget(document: Document) = document.select(".tags span").map {
+        it.text().trim()
+    }.joinToString(", ")
+    override fun statusget(document: Document) = when (document.select(".type").text().substringAfter("：").trim()) {
+        "连载中" -> SManga.ONGOING
+        //"Completed" -> SManga.COMPLETED
+        else -> SManga.UNKNOWN
+    }
+}
+
+class TwinsComicszh: BoomManga("TwinsComics", "https://m.twinscomics.com/zh", "zh"){
+    override fun nameselector(element: Element) = element.select("a").attr("alt")
+    override fun authorget(document: Document) = document.select(".author").text().substringAfter("：").trim()
+    override fun thumbnailget(document: Document) = document.select(".bg-box .bg").attr("style").substringAfter("'").substringBefore("'")
+    override fun genreget(document: Document) = document.select(".tags span").map {
+        it.text().trim()
+    }.joinToString(", ")
+    override fun statusget(document: Document) = when (document.select(".type").text().substringAfter("：").trim()) {
+        "连载中" -> SManga.ONGOING
+        //"Completed" -> SManga.COMPLETED
         else -> SManga.UNKNOWN
     }
 }
