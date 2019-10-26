4347bbb5e4fbaf9190c130332f8255dc4c6a3388
snakedoc83 <mikepeterson83@gmail.com>
2019-10-25 21:34:00 -0400

    headers

--- src/all/fmreader/src/eu/kanade/tachiyomi/extension/all/fmreader/FMReaderFactory.kt
+++ src/all/fmreader/src/eu/kanade/tachiyomi/extension/all/fmreader/FMReaderFactory.kt
@@ -241,7 +241,7 @@ class Manhwa18Net : FMReader("Manhwa18.net", "https://manhwa18.net", "en") {
     override fun popularMangaRequest(page: Int): Request =
         GET("$baseUrl/$requestPath?listType=pagination&page=$page&sort=views&sort_type=DESC&ungenre=raw", headers)
     override fun latestUpdatesRequest(page: Int): Request =
-        GET("$baseUrl/$requestPath?listType=pagination&page=$page&sort=last_update&sort_type=DESC&ungenre=raw")
+        GET("$baseUrl/$requestPath?listType=pagination&page=$page&sort=last_update&sort_type=DESC&ungenre=raw", headers)
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val noRawsUrl = super.searchMangaRequest(page, query, filters).url().newBuilder().addQueryParameter("ungenre", "raw").toString()
         return GET(noRawsUrl, headers)
