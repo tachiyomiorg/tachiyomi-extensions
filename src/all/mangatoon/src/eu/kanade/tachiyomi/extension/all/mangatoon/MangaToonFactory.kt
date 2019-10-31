65f8e97d7adad117db467d7c5c372304ed619e3f
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2019-10-30 23:48:58 -0400

    Update MangaToonFactory.kt

--- src/all/mangatoon/src/eu/kanade/tachiyomi/extension/all/mangatoon/MangaToonFactory.kt
+++ src/all/mangatoon/src/eu/kanade/tachiyomi/extension/all/mangatoon/MangaToonFactory.kt
@@ -2,10 +2,6 @@ package eu.kanade.tachiyomi.extension.all.mangatoon
 
 import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.SourceFactory
-import eu.kanade.tachiyomi.source.model.SManga
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-
 
 class MangaToonFactory : SourceFactory {
     override fun createSources(): List<Source> = listOf(
@@ -18,14 +14,11 @@ class MangaToonFactory : SourceFactory {
         TH()
     )
 
-
-class ZH : MangaToon("MangaToon (Limited)", "https://mangatoon.mobi", "zh", "cn")
-class EN : MangaToon("MangaToon (Limited)", "https://mangatoon.mobi", "en", "en")
-class ID : MangaToon("MangaToon (Limited)", "https://mangatoon.mobi", "id", "id")
-class VI : MangaToon("MangaToon (Limited)", "https://mangatoon.mobi", "vi", "vi")
-class ES : MangaToon("MangaToon (Limited)", "https://mangatoon.mobi", "es", "es")
-class PT : MangaToon("MangaToon (Limited)", "https://mangatoon.mobi", "pt", "pt")
-class TH : MangaToon("MangaToon (Limited)", "https://mangatoon.mobi", "th", "th")
-
-
+class ZH : MangaToon("zh", "cn")
+class EN : MangaToon("en", "en")
+class ID : MangaToon("id", "id")
+class VI : MangaToon("vi", "vi")
+class ES : MangaToon("es", "es")
+class PT : MangaToon("pt", "pt")
+class TH : MangaToon("th", "th")
 }
