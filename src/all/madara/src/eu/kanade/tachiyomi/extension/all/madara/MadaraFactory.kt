6b3851e2781610a5bd96c5bd40194d762a5aa7a9
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2020-01-10 16:55:37 -0500

    Update MadaraFactory.kt

--- src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
+++ src/all/madara/src/eu/kanade/tachiyomi/extension/all/madara/MadaraFactory.kt
@@ -220,7 +220,6 @@ class GetManhwa : Madara("GetManhwa", "https://getmanhwa.co", "en")
 class AllPornComic : Madara("AllPornComic", "https://allporncomic.com", "en") {
     override fun headersBuilder(): Headers.Builder = Headers.Builder()
         .add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.${Random().nextInt(99)} (KHTML, like Gecko) Chrome/79.0.${Random().nextInt(9999)}.${Random().nextInt(999)} Safari/537.${Random().nextInt(99)}")
-        .add("Referer", baseUrl)
     override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/manga/page/$page/?m_orderby=views", headers)
     override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/manga/page/$page/?m_orderby=latest", headers)
     override fun searchMangaNextPageSelector() = "a[rel=next]"
