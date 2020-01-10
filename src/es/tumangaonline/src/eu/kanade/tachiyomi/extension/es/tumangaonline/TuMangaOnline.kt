d7653ab354b43c3a5a468a3cbf144b597c3383cf
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2020-01-10 13:40:50 -0500

    Update TuMangaOnline.kt

--- src/es/tumangaonline/src/eu/kanade/tachiyomi/extension/es/tumangaonline/TuMangaOnline.kt
+++ src/es/tumangaonline/src/eu/kanade/tachiyomi/extension/es/tumangaonline/TuMangaOnline.kt
@@ -294,7 +294,7 @@ class TuMangaOnline : ConfigurableSource, ParsedHttpSource() {
         val chapterID = response.request().url().toString().substringAfter("viewer/").substringBefore("/cascade")
         val body = response.asJsoup()
         val script = body.select("script:containsData($scriptselector)").html()
-        val pages = body.select("div#viewer-container > div.viewer-image-container > .viewer-image").map { script.substringAfter("_${it.attr("id")}.src = '").substringBefore("';") }
+        val pages = body.select(".viewer-image:eq(0)").map {it.attr("id")}.map { script.substringAfter("_${it.substring(0,it.length-1)}.src = '").substringBefore("';") }
         Log.i("TachiDebug","Script Check => $script") //TODO - Reminder to remove log
         pages.forEach {
             Log.i("TachiDebug","Page URL => $it") //TODO - Reminder to remove log
