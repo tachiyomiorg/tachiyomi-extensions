5dd48c81332698527c98aeadc3a1ec31f784c72a
Alessandro Jean <alessandrojean@gmail.com>
2020-03-09 21:57:17 -0300

    Change user agent to Windows.

--- src/all/mangaplus/src/eu/kanade/tachiyomi/extension/all/mangaplus/MangaPlus.kt
+++ src/all/mangaplus/src/eu/kanade/tachiyomi/extension/all/mangaplus/MangaPlus.kt
@@ -296,7 +296,7 @@ abstract class MangaPlus(override val lang: String,
     companion object {
         private const val WEB_URL = "https://mangaplus.shueisha.co.jp"
         private const val IMAGES_WESERV_URL = "https://images.weserv.nl"
-        private const val USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
+        private const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
 
         private val HEX_GROUP = "(.{1,2})".toRegex()
 
