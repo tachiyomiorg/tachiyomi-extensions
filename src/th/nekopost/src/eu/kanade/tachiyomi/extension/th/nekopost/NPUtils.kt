742e517740b0eff36480bb52bd699f64a79dd987
Sittikorn Hirunpongthawat <buskun.sittikorn@gmail.com>
2020-04-12 03:36:16 +0700

    Add Nekopost V.1.2.1

---
+++ src/th/nekopost/src/eu/kanade/tachiyomi/extension/th/nekopost/NPUtils.kt
@@ -0,0 +1,76 @@
+package eu.kanade.tachiyomi.extension.th.nekopost
+
+import java.net.URLEncoder
+import java.text.SimpleDateFormat
+import java.util.*
+
+object NPUtils {
+    private val urlWithoutDomainFromFullUrlRegex: Regex
+        get() = Regex("^https://www\\.nekopost\\.net/manga/(.*)$")
+
+
+    fun getUrlWithoutDomainFromFullUrl(url: String): String {
+        val (urlWithoutDomain) = urlWithoutDomainFromFullUrlRegex.find(url)!!.destructured
+        return urlWithoutDomain
+    }
+
+    fun convertDateStringToEpoch(dateStr: String, format: String = "yyyy-MM-dd"): Long = SimpleDateFormat(format, Locale("th")).parse(dateStr).time
+
+    fun getSearchQuery(keyword: String = "", genreList: Array<Genre>, statusList: Array<Status>): String {
+        val keywordQuery = "ip_keyword=$keyword"
+
+        val genreQuery = genreList.joinToString("&") { genre -> "ip_genre[]=${genre.genre}" }
+
+        val statusQuery = statusList.let {
+            if (it.isNotEmpty()) it
+            else Status.values()
+        }.joinToString("&") { status -> "ip_status[]=${status.status}" }
+
+        val typeQuery = "ip_type[]=m"
+
+        return "$keywordQuery&$genreQuery&$statusQuery&$typeQuery"
+    }
+
+    enum class Genre(val title: String, val genre: Int) {
+        ACTION("Action", 2),
+        ADVENTURE("Adventure", 13),
+        COMEDY("Comedy", 8),
+        DOUJINSHI("Doujinshi", 37),
+        DRAMA("Drama", 3),
+        FANTASY("Fantasy", 1),
+        GENDER_BENDER("Gender Bender", 26),
+        GRUME("Grume", 41),
+        HORROR("Horror", 47),
+        ISEKAI("Isekai", 44),
+        MYSTERY("Mystery", 32),
+        ONE_SHORT("One short", 48),
+        ROMANCE("Romance", 10),
+        SCHOOL_LIFE("School Life", 43),
+        SCI_FI("Sci-fi", 7),
+        SECOND_LIFE("Second Life", 45),
+        SEINEN("Seinen", 49),
+        SHOUJO("Shoujo", 42),
+        SHOUNEN("Shounen", 46),
+        SLICE_OF_LIFE("Slice of Life", 9),
+        SPORT("Sport", 5),
+        TRAP("Trap", 25),
+        YAOI("Yaoi", 23),
+        YURI("Yuri", 24);
+
+        companion object {
+            fun getGenre(title: String): Genre? = values().find { genre -> genre.title == title }
+        }
+    }
+
+    enum class Status(val title: String, val status: Int) {
+        ONGOING("Ongoing", 1),
+        COMPLETED("Completed", 2),
+        LICENSED("Licensed", 3);
+
+        companion object {
+            fun getStatus(title: String): Status? = values().find { Status -> Status.title == title }
+        }
+    }
+
+    val monthMap: Array<String> = arrayOf("JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC")
+}
---
+++ src/th/nekopost/src/eu/kanade/tachiyomi/extension/th/nekopost/Nekopost.kt
@@ -0,0 +1,222 @@
+package eu.kanade.tachiyomi.extension.th.nekopost
+
+import android.util.Log
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.*
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.net.URL
+import java.util.*
+import kotlin.collections.ArrayList
+
+class Nekopost() : ParsedHttpSource() {
+    override val baseUrl: String
+        get() = "https://www.nekopost.net/manga/"
+    private val mangaListUrl: String
+        get() = "https://www.nekopost.net/project/ajax_load_update/m/"
+    private val chapterContentUrl: String
+        get() = "https://www.nekopost.net/reader/loadChapterContent/"
+    private val chapterImageUrl: String
+        get() = "https://www.nekopost.net/file_server/collectManga/"
+    private val fallbackImageUrl: String
+        get() = "https://www.nekopost.net/images/no_image.jpg"
+    private val searchUrl: String
+        get() = "https://www.nekopost.net/search/"
+    override val lang: String
+        get() = "th"
+    override val name: String
+        get() = "Nekopost"
+    override val supportsLatest: Boolean
+        get() = true
+
+    override fun chapterListSelector(): String = ".bg-card.card.pb-2 tr"
+
+    override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
+        element.select("a").first().let {
+            setUrlWithoutDomain(NPUtils.getUrlWithoutDomainFromFullUrl(it.attr("href")))
+            name = it.text()
+        }
+        date_upload = NPUtils.convertDateStringToEpoch(element.select("b").last().nextSibling().toString().trim())
+        scanlator = element.select("a").last().text()
+    }
+
+    override fun imageUrlParse(document: Document): String = ".bg-card.card .p-3.text-white img"
+
+    override fun latestUpdatesParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+
+        val mangas = document.select(latestUpdatesSelector()).filter { element ->
+            val dateText = element.select(".date").text().trim()
+            val currentDate = Calendar.getInstance(Locale("th"))
+
+            dateText.contains(currentDate.get(Calendar.DATE).toString()) && dateText.contains(NPUtils.monthMap[currentDate.get(Calendar.MONTH)])
+        }.map { element -> latestUpdatesFromElement(element) }.distinctBy { manga -> manga.url }
+
+        val hasNextPage = mangas.isNotEmpty()
+
+        return MangasPage(mangas, hasNextPage)
+    }
+
+    override fun latestUpdatesFromElement(element: Element): SManga = SManga.create().apply {
+        setUrlWithoutDomain(NPUtils.getUrlWithoutDomainFromFullUrl(element.select("a").attr("href")))
+        title = element.select(".info > b").text().trim()
+        thumbnail_url = element.select(".img img").first().attr("src").replace("preview", "cover").let { url ->
+            if (url === "") fallbackImageUrl
+            else url
+        }
+    }
+
+    override fun latestUpdatesNextPageSelector(): String? = "*"
+
+    override fun latestUpdatesRequest(page: Int): Request = GET("$mangaListUrl/${page - 1}")
+
+    override fun latestUpdatesSelector(): String = "a[href]"
+
+    override fun mangaDetailsParse(document: Document): SManga = SManga.create().apply {
+        document.select(".bg-card.card").first().let {
+            title = it.select(".card-title.text-silver").text()
+            thumbnail_url = it.select(".bg-card.card").select(".p-3.text-white").select("img").first().attr("src").let { url ->
+                if (url === "") fallbackImageUrl
+                else url
+            }
+
+            it.select("table.mt-1").select("tr").let { tr ->
+                author = tr[0].select("td").last().text()
+                artist = tr[1].select("td").last().text()
+                status = when (tr[3].select("td").last().text()) {
+                    "Active" -> SManga.ONGOING
+                    "Completed" -> SManga.COMPLETED
+                    "Licensed" -> SManga.LICENSED
+                    else -> SManga.UNKNOWN
+                }
+            }
+
+            description = it.select(".bg-secondary").text().trim()
+            genre = it.select("td[colspan='2'][valign='top']").first().select(".text-silver").first().nextSibling().toString().trim()
+        }
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        return JSONArray(URL("$chapterContentUrl${NPUtils.getUrlWithoutDomainFromFullUrl(document.location())}").readText()).let { chapterContentJSON ->
+            try {
+                val pageListJSON = chapterContentJSON.getJSONArray(3)
+                val chapterDataJson = chapterContentJSON.getJSONObject(1)
+
+                val pageList: ArrayList<Page> = ArrayList()
+
+                for (i in 0 until pageListJSON.length()) {
+                    pageList.add(
+                        Page(i, "", pageListJSON.getJSONObject(i).let { pageJSON ->
+                            "$chapterImageUrl${chapterDataJson.getString("nc_project_id")}/${pageJSON.getString("chapter_id")}/${pageJSON.getString("value_url")}"
+                        })
+                    )
+                }
+
+                pageList
+            } catch (e: JSONException) {
+                val pageListNameJSON = chapterContentJSON.getString(3)
+                val chapterDataJson = chapterContentJSON.getJSONObject(1)
+
+                val pageListDataJSON = JSONObject(URL("$chapterImageUrl${chapterDataJson.getString("nc_project_id")}/${chapterDataJson.getString("nc_chapter_id")}/$pageListNameJSON").readText())
+                val pageListJSON = pageListDataJSON.getJSONArray("pageItem")
+
+                val pageList: ArrayList<Page> = ArrayList()
+
+                for (i in 0 until pageListJSON.length()) {
+                    pageList.add(
+                        Page(i, "", pageListJSON.getJSONObject(i).let { pageJSON ->
+                            "$chapterImageUrl${chapterDataJson.getString("nc_project_id")}/${chapterDataJson.getString("nc_chapter_id")}/${pageJSON.getString("fileName")}"
+                        })
+                    )
+                }
+
+                pageList
+            }
+        }
+    }
+
+    override fun popularMangaParse(response: Response): MangasPage {
+        val document = response.asJsoup()
+
+        val mangas = document.select(popularMangaSelector()).map { element -> popularMangaFromElement(element) }.distinctBy { manga -> manga.url }
+
+        val hasNextPage = true
+
+        return MangasPage(mangas, hasNextPage)
+    }
+
+    override fun popularMangaFromElement(element: Element): SManga = latestUpdatesFromElement(element)
+
+    override fun popularMangaNextPageSelector(): String? = latestUpdatesNextPageSelector()
+
+    override fun popularMangaRequest(page: Int): Request = latestUpdatesRequest(page)
+
+    override fun popularMangaSelector(): String = latestUpdatesSelector()
+
+    override fun getFilterList(): FilterList = FilterList(
+        GenreFilter(),
+        StatusFilter()
+    )
+
+    private class GenreFilter : Filter.Group<GenreCheckbox>("Genre", NPUtils.Genre.values().map { genre -> GenreCheckbox(genre) })
+
+    private class GenreCheckbox(genre: NPUtils.Genre) : Filter.CheckBox(genre.title, false)
+
+    private class StatusFilter : Filter.Group<StatusCheckbox>("Status", NPUtils.Status.values().map { status -> StatusCheckbox(status) })
+
+    private class StatusCheckbox(status: NPUtils.Status) : Filter.CheckBox(status.title, false)
+
+    override fun searchMangaFromElement(element: Element): SManga {
+        Log.v("Nekopost", element.select("img").attr("data-original"))
+        return SManga.create().apply {
+            element.select(".project_info").select("a").let {
+                title = it.text()
+                setUrlWithoutDomain(NPUtils.getUrlWithoutDomainFromFullUrl(it.attr("href")))
+            }
+            thumbnail_url = element.select("img").attr("data-original").let { url ->
+                if (url === "") fallbackImageUrl
+                else url
+            }
+
+            status = when (element.select(".status").text()) {
+                "On Going" -> SManga.ONGOING
+                "Completed" -> SManga.COMPLETED
+                "Licensed" -> SManga.LICENSED
+                else -> SManga.UNKNOWN
+            }
+        }
+    }
+
+    override fun searchMangaNextPageSelector(): String? = null
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        if (page > 1) throw Error("No more page")
+
+        val genreList: Array<NPUtils.Genre> = try {
+            (filters.find { filter -> filter is GenreFilter } as GenreFilter).state.filter { checkbox -> checkbox.state }.map { checkbox ->
+                NPUtils.Genre.getGenre(checkbox.name)!!
+            }.toTypedArray()
+        } catch (e: Exception) {
+            emptyArray()
+        }
+
+        val statusList: Array<NPUtils.Status> = try {
+            (filters.find { filter -> filter is StatusFilter } as StatusFilter).state.filter { checkbox -> checkbox.state }.map { checkbox ->
+                NPUtils.Status.getStatus(checkbox.name)!!
+            }.toTypedArray()
+        } catch (e: Exception) {
+            emptyArray()
+        }
+
+        return GET("$searchUrl?${NPUtils.getSearchQuery(query, genreList, statusList)}")
+    }
+
+    override fun searchMangaSelector(): String = ".list_project .item"
+}
