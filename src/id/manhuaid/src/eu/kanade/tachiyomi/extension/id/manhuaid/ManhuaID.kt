e39363bcacb6b76d43c2353bfec97f4cbfb51a68
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2020-04-14 23:18:12 +0100

    Edit ManhuaID.kt

--- src/id/manhuaid/src/eu/kanade/tachiyomi/extension/id/manhuaid/ManhuaID.kt
+++ src/id/manhuaid/src/eu/kanade/tachiyomi/extension/id/manhuaid/ManhuaID.kt
@@ -7,7 +7,6 @@ import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
-import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
