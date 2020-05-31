2d6d85a0e1a600d38b270c3da44a0f079903876b
e-shl <e.shlyapkin99@gmail.com>
2020-05-31 21:35:51 +0500

    Filters

--- src/ru/remanga/src/eu/kanade/tachiyomi/extension/ru/remanga/Remanga.kt
+++ src/ru/remanga/src/eu/kanade/tachiyomi/extension/ru/remanga/Remanga.kt
@@ -12,6 +12,7 @@ import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.Gson
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.asObservableSuccess
+import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
@@ -27,7 +28,6 @@ import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.Jsoup
 import rx.Observable
-
 class Remanga : HttpSource() {
     override val name = "Remanga"
 
@@ -85,7 +85,37 @@ class Remanga : HttpSource() {
         }
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
-        val url = HttpUrl.parse("$baseUrl/api/search/?query=$query&page=$page")!!.newBuilder()
+        var url = HttpUrl.parse("$baseUrl/api/search/catalog/?page=$page")!!.newBuilder()
+        if (query.isNotEmpty()) {
+            url = HttpUrl.parse("$baseUrl/api/search/?page=$page")!!.newBuilder()
+            url.addQueryParameter("query", query)
+        }
+        (if (filters.isEmpty()) getFilterList() else filters).forEach { filter ->
+            when (filter) {
+                is CategoryList -> filter.state.forEach { category ->
+                    if (category.state != Filter.TriState.STATE_IGNORE) {
+                        url.addQueryParameter(if (category.isIncluded()) "types" else "exclude_types", category.id)
+                    }
+                }
+                is StatusList -> filter.state.forEach { status ->
+                    if (status.state != false) {
+                        url.addQueryParameter("status", status.id)
+                    }
+                }
+                is GenreList -> filter.state.forEach { genre ->
+                    if (genre.state != Filter.TriState.STATE_IGNORE) {
+                        url.addQueryParameter(if (genre.isIncluded()) "genres" else "exclude_genres", genre.id)
+                    }
+                }
+                is OrderBy -> {
+                    var ord = arrayOf("id", "chapter_date", "rating", "votes", "views", "random")[filter.state!!.index]
+                    if (!filter.state!!.ascending) {
+                        ord = "-" + ord
+                    }
+                    url.addQueryParameter("ordering", ord)
+                }
+            }
+        }
         return GET(url.toString(), headers)
     }
 
@@ -176,6 +206,101 @@ class Remanga : HttpSource() {
             Page(it.page, "", it.link)
         }
     }
+    private class SearchFilter(name: String, val id: String) : Filter.TriState(name)
+    private class CheckFilter(name: String, val id: String) : Filter.CheckBox(name)
+
+    private class CategoryList(categories: List<SearchFilter>) : Filter.Group<SearchFilter>("Категории", categories)
+    private class StatusList(statuses: List<CheckFilter>) : Filter.Group<CheckFilter>("Статус", statuses)
+    private class GenreList(genres: List<SearchFilter>) : Filter.Group<SearchFilter>("Жанры", genres)
+
+    override fun getFilterList() = FilterList(
+        CategoryList(getCategoryList()),
+        StatusList(getStatusList()),
+        GenreList(getGenreList()),
+        OrderBy()
+    )
+
+    private class OrderBy : Filter.Sort("Сортировка",
+        arrayOf("Новизне", "Последним обновлениям", "Популярности", "Лайкам", "Просмотрам", "Мне повезет"),
+        Selection(2, false))
+
+    /*
+    * Use console
+    * Object.entries(__FILTER_ITEMS__.types).map(([k, v]) => `SearchFilter("${v.label}", "${v.id}")`).join(',\n')
+    * on /manga-list
+    */
+    private fun getCategoryList() = listOf(
+        SearchFilter("Манга", "0"),
+        SearchFilter("Манхва", "1"),
+        SearchFilter("Маньхуа", "2"),
+        SearchFilter("Западный комикс", "3"),
+        SearchFilter("Русскомикс", "4"),
+        SearchFilter("Индонезийский комикс", "5"),
+        SearchFilter("Новелла", "6"),
+        SearchFilter("Другое", "7")
+    )
+
+    /*
+    * Use console
+    * Object.entries(__FILTER_ITEMS__.status).map(([k, v]) => `SearchFilter("${v.label}", "${v.id}")`).join(',\n')
+    * on /manga-list
+    */
+    private fun getStatusList() = listOf(
+        CheckFilter("Закончен", "0"),
+        CheckFilter("Продолжается", "1"),
+        CheckFilter("Заморожен", "2")
+    )
+
+    /*
+    * Use console
+    * __FILTER_ITEMS__.genres.map(it => `SearchFilter("${it.name}", "${it.id}")`).join(',\n')
+    * on /manga-list
+    */
+    private fun getGenreList() = listOf(
+        SearchFilter("арт", "1"),
+        SearchFilter("бдсм", "44"),
+        SearchFilter("боевик", "2"),
+        SearchFilter("боевые искусства", "3"),
+        SearchFilter("вампиры", "4"),
+        SearchFilter("гарем", "5"),
+        SearchFilter("гендерная интрига", "6"),
+        SearchFilter("героическое фэнтези", "7"),
+        SearchFilter("детектив", "8"),
+        SearchFilter("дзёсэй", "9"),
+        SearchFilter("додзинси", "10"),
+        SearchFilter("драма", "11"),
+        SearchFilter("игра", "12"),
+        SearchFilter("история", "13"),
+        SearchFilter("киберпанк", "14"),
+        SearchFilter("кодомо", "15"),
+        SearchFilter("комедия", "16"),
+        SearchFilter("махо-сёдзё", "17"),
+        SearchFilter("меха", "18"),
+        SearchFilter("мистика", "19"),
+        SearchFilter("научная фантастика", "20"),
+        SearchFilter("повседневность", "21"),
+        SearchFilter("постапокалиптика", "22"),
+        SearchFilter("приключения", "23"),
+        SearchFilter("психология", "24"),
+        SearchFilter("романтика", "25"),
+        SearchFilter("сверхъестественное", "27"),
+        SearchFilter("сёдзё", "28"),
+        SearchFilter("сёдзё-ай", "29"),
+        SearchFilter("сёнэн", "30"),
+        SearchFilter("сёнэн-ай", "31"),
+        SearchFilter("спорт", "32"),
+        SearchFilter("сэйнэн", "33"),
+        SearchFilter("трагедия", "34"),
+        SearchFilter("триллер", "35"),
+        SearchFilter("ужасы", "36"),
+        SearchFilter("фантастика", "37"),
+        SearchFilter("фэнтези", "38"),
+        SearchFilter("школа", "39"),
+        SearchFilter("эротика", "42"),
+        SearchFilter("этти", "40"),
+        SearchFilter("юри", "41"),
+        SearchFilter("яой", "43")
+    )
 
     private val gson by lazy { Gson() }
 }
