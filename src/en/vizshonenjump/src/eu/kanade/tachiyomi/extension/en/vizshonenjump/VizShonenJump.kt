fb222ddeb9654dee9cb8b279ba603f97f9fe2828
Alessandro Jean <alessandrojean@gmail.com>
2020-06-18 11:04:13 -0300

    Add Viz Shonen Jump.

---
+++ src/en/vizshonenjump/src/eu/kanade/tachiyomi/extension/en/vizshonenjump/VizShonenJump.kt
@@ -0,0 +1,225 @@
+package eu.kanade.tachiyomi.extension.en.vizshonenjump
+
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Locale
+import okhttp3.Headers
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import rx.Observable
+
+class VizShonenJump : ParsedHttpSource() {
+
+    override val name = "VIZ Shonen Jump"
+
+    override val baseUrl = "https://www.viz.com"
+
+    override val lang = "en"
+
+    override val supportsLatest = true
+
+    override val client: OkHttpClient = network.client.newBuilder()
+        .addInterceptor(VizImageInterceptor())
+        .build()
+
+    override fun headersBuilder(): Headers.Builder = Headers.Builder()
+        .add("User-Agent", USER_AGENT)
+        .add("Origin", baseUrl)
+        .add("Referer", "$baseUrl/shonenjump")
+
+    override fun popularMangaRequest(page: Int): Request {
+        val newHeaders = headersBuilder()
+            .set("Referer", baseUrl)
+            .build()
+
+        return GET("$baseUrl/shonenjump", newHeaders)
+    }
+
+    override fun popularMangaParse(response: Response): MangasPage {
+        val mangas = super.popularMangaParse(response).mangas
+
+        if (mangas.isEmpty())
+            throw Exception(COUNTRY_NOT_SUPPORTED)
+
+        return MangasPage(mangas.sortedBy { it.title }, false)
+    }
+
+    override fun popularMangaSelector(): String = "section.section_chapters div.o_sort_container div.o_sortable a"
+
+    override fun popularMangaFromElement(element: Element): SManga = SManga.create().apply {
+        title = element.select("div.pad-x-rg").first().text()
+        thumbnail_url = element.select("div.pos-r img.disp-bl").first()?.attr("data-original")
+        url = element.attr("href")
+    }
+
+    override fun popularMangaNextPageSelector(): String? = null
+
+    override fun latestUpdatesRequest(page: Int): Request = popularMangaRequest(page)
+
+    override fun latestUpdatesParse(response: Response): MangasPage {
+        val mangasPage = super.latestUpdatesParse(response)
+
+        if (mangasPage.mangas.isEmpty())
+            throw Exception(COUNTRY_NOT_SUPPORTED)
+
+        return mangasPage
+    }
+
+    override fun latestUpdatesSelector() = popularMangaSelector()
+
+    override fun latestUpdatesFromElement(element: Element): SManga = popularMangaFromElement(element)
+
+    override fun latestUpdatesNextPageSelector(): String? = null
+
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> {
+        return super.fetchSearchManga(page, query, filters)
+            .map { MangasPage(it.mangas.filter { m -> m.title.contains(query, true) }, it.hasNextPage) }
+    }
+
+    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request = popularMangaRequest(page)
+
+    override fun searchMangaParse(response: Response): MangasPage {
+        val mangasPage = super.searchMangaParse(response)
+
+        if (mangasPage.mangas.isEmpty())
+            throw Exception(COUNTRY_NOT_SUPPORTED)
+
+        return mangasPage
+    }
+
+    override fun searchMangaSelector() = popularMangaSelector()
+
+    override fun searchMangaFromElement(element: Element): SManga = popularMangaFromElement(element)
+
+    override fun searchMangaNextPageSelector(): String? = null
+
+    override fun mangaDetailsParse(document: Document): SManga {
+        val seriesIntro = document.select("section#series-intro").first()
+
+        return SManga.create().apply {
+            author = seriesIntro.select("div.type-rg span").first()?.text()
+                ?.replace("Created by ", "")
+            artist = author
+            status = SManga.ONGOING
+            description = seriesIntro.select("h4").first().text()
+        }
+    }
+
+    override fun chapterListSelector() =
+        "section.section_chapters div.o_sortable > a.o_chapter-container:not([href*=javascript]), " +
+            "section.section_chapters div.o_sortable div.o_chapter-vol-container tr.o_chapter a.o_chapter-container.pad-r-0:not([href*=javascript])"
+
+    override fun chapterFromElement(element: Element): SChapter = SChapter.create().apply {
+        val isVolume = element.select("div:nth-child(1) table").first() == null
+
+        if (isVolume) {
+            name = element.text()
+        } else {
+            val leftSide = element.select("div:nth-child(1) table").first()!!
+            val rightSide = element.select("div:nth-child(2) table").first()!!
+
+            name = rightSide.select("td.ch-num-list-spacing").first()!!.text()
+            date_upload = DATE_FORMATTER.tryParseTime(leftSide.select("td[align=right]").first()!!.text())
+        }
+
+        scanlator = "VIZ Media"
+        url = element.attr("href")
+    }
+
+    override fun pageListRequest(chapter: SChapter): Request {
+        val newHeaders = headersBuilder()
+            .set("Referer", baseUrl + chapter.url.substringBefore("-chapter"))
+            .build()
+
+        return GET(baseUrl + chapter.url, newHeaders)
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        val pageCount = document.select("script:containsData(var pages)").first().data()
+            .substringAfter("= ")
+            .substringBefore(";")
+            .toInt()
+        val mangaId = document.location()
+            .substringAfterLast("/")
+            .substringBefore("?")
+
+        return IntRange(1, pageCount)
+            .map {
+                val imageUrl = HttpUrl.parse("$baseUrl/manga/get_manga_url")!!.newBuilder()
+                    .addQueryParameter("device_id", "3")
+                    .addQueryParameter("manga_id", mangaId)
+                    .addQueryParameter("page", it.toString())
+                    .addEncodedQueryParameter("referer", document.location())
+                    .toString()
+
+                Page(it, imageUrl)
+            }
+    }
+
+    override fun imageUrlRequest(page: Page): Request {
+        val url = HttpUrl.parse(page.url)!!
+        val referer = url.queryParameter("referer")!!
+        val newUrl = url.newBuilder()
+            .removeAllEncodedQueryParameters("referer")
+            .toString()
+
+        val newHeaders = headersBuilder()
+            .add("X-Requested-With", "XMLHttpRequest")
+            .set("Referer", referer)
+            .build()
+
+        return GET(newUrl, newHeaders)
+    }
+
+    override fun imageUrlParse(response: Response): String {
+        val cdnUrl = response.body()!!.string()
+        val referer = response.request().header("Referer")!!
+
+        return HttpUrl.parse(cdnUrl)!!.newBuilder()
+            .addEncodedQueryParameter("referer", referer)
+            .toString()
+    }
+
+    override fun imageUrlParse(document: Document) = ""
+
+    override fun imageRequest(page: Page): Request {
+        val imageUrl = HttpUrl.parse(page.imageUrl!!)!!
+        val referer = imageUrl.queryParameter("referer")!!
+        val newImageUrl = imageUrl.newBuilder()
+            .removeAllEncodedQueryParameters("referer")
+            .toString()
+
+        val newHeaders = headersBuilder()
+            .set("Referer", referer)
+            .build()
+
+        return GET(newImageUrl, newHeaders)
+    }
+
+    private fun SimpleDateFormat.tryParseTime(date: String): Long {
+        return try {
+            parse(date).time
+        } catch (e: ParseException) {
+            0L
+        }
+    }
+
+    companion object {
+        private const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36"
+
+        private val DATE_FORMATTER by lazy { SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH) }
+
+        private const val COUNTRY_NOT_SUPPORTED = "Your country is not supported, try using a VPN."
+    }
+}
