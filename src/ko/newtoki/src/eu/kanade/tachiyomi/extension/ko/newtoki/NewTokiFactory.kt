3328f7fe8f51b09584bbea1616b29c511751ed25
DitFranXX <DitF@protonmail.com>
2020-09-02 16:59:42 +0900

    Update NewToki Extension * Site name/domain changes  - ManaMoa merged into NewToki's manga side. calls ManaToki.  - Site wasn't not changed too hard so decided to keep old site ids.  * Fix issue with pages with optional <p> tags

--- src/ko/newtoki/src/eu/kanade/tachiyomi/extension/ko/newtoki/NewTokiFactory.kt
+++ src/ko/newtoki/src/eu/kanade/tachiyomi/extension/ko/newtoki/NewTokiFactory.kt
@@ -6,6 +6,7 @@ import eu.kanade.tachiyomi.source.SourceFactory
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
+import java.security.MessageDigest
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -17,6 +18,10 @@ import org.jsoup.nodes.Document
 /**
  * Source changes domain names every few days (e.g. newtoki31.net to newtoki32.net)
  * The domain name was newtoki32 on 2019-11-14, this attempts to match the rate at which the domain changes
+ *
+ * Since 2020-09-20, They changed manga side to Manatoki.
+ * It was merged after shutdown of ManaMoa.
+ * This is by the head of Manamoa, as thety decided to move to Newtoki.
  */
 private val domainNumber = 32 + ((Date().time - SimpleDateFormat("yyyy-MM-dd", Locale.US).parse("2019-11-14")!!.time) / 595000000)
 
@@ -27,9 +32,25 @@ class NewTokiFactory : SourceFactory {
     )
 }
 
-class NewTokiManga : NewToki("NewToki", "https://newtoki$domainNumber.net", "comic")
+class NewTokiManga : NewToki("ManaToki", "https://manatoki$domainNumber.net", "comic") {
+    override val id by lazy {
+        // / ! DO NOT CHANGE THIS !  Only the site name changed from newtoki.
+        val oldName = "NewToki"
+        val key = "${oldName.toLowerCase()}/$lang/$versionId"
+        val bytes = MessageDigest.getInstance("MD5").digest(key.toByteArray())
+        (0..7).map { bytes[it].toLong() and 0xff shl 8 * (7 - it) }.reduce(Long::or) and Long.MAX_VALUE
+    }
+}
 
-class NewTokiWebtoon : NewToki("NewToki (Webtoon)", "https://newtoki$domainNumber.com", "webtoon") {
+class NewTokiWebtoon : NewToki("NewToki", "https://newtoki$domainNumber.com", "webtoon") {
+    override val id by lazy {
+        // / ! DO NOT CHANGE THIS !  Prevent to treating as a new site
+        val oldName = "NewToki (Webtoon)"
+        val key = "${oldName.toLowerCase()}/$lang/$versionId"
+        val bytes = MessageDigest.getInstance("MD5").digest(key.toByteArray())
+        (0..7).map { bytes[it].toLong() and 0xff shl 8 * (7 - it) }.reduce(Long::or) and Long.MAX_VALUE
+    }
+
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val url = HttpUrl.parse("$baseUrl/webtoon" + (if (page > 1) "/p$page" else ""))!!.newBuilder()
         filters.forEach { filter ->
