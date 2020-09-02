7ec0fd95154d70f4388d5ce940cd107bd807e21a
snakedoc83 <mikepeterson83@gmail.com>
2020-09-01 21:33:04 -0400

    Hatigarm versionId

--- src/en/hatigarmscans/src/eu/kanade/tachiyomi/extension/en/hatigarmscans/HatigarmScans.kt
+++ src/en/hatigarmscans/src/eu/kanade/tachiyomi/extension/en/hatigarmscans/HatigarmScans.kt
@@ -2,4 +2,6 @@ package eu.kanade.tachiyomi.extension.en.hatigarmscans
 
 import eu.kanade.tachiyomi.lib.themesources.genkan.Genkan
 
-class HatigarmScans : Genkan("Hatigarm Scans", "https://hatigarmscanz.net", "en")
+class HatigarmScans : Genkan("Hatigarm Scans", "https://hatigarmscanz.net", "en") {
+    override val versionId = 2
+}
