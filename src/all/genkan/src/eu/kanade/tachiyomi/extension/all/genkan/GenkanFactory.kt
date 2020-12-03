2d616566b302f588bc358f0c72fd627bbf3cb39d
sam7028 <69104545+sam7028@users.noreply.github.com>
2020-12-03 16:58:07 +0530

    Revert "Remove KKJ Scans"
    This reverts commit 2d9957f4f265d7ae46d6f7b9f1bcd2213932af20.

--- src/all/genkan/src/eu/kanade/tachiyomi/extension/all/genkan/GenkanFactory.kt
+++ src/all/genkan/src/eu/kanade/tachiyomi/extension/all/genkan/GenkanFactory.kt
@@ -16,6 +16,7 @@ class GenkanFactory : SourceFactory {
         SecretScans(),
         MethodScans(),
         SKScans(),
+        KKJScans(),
         KrakenScans()
     )
 }
@@ -37,4 +38,5 @@ class EdelgardeScans : Genkan("Edelgarde Scans", "https://edelgardescans.com", "
 class SecretScans : GenkanOriginal("SecretScans", "https://secretscans.co", "en")
 class MethodScans : Genkan("Method Scans", "https://methodscans.com", "en")
 class SKScans : Genkan("Sleeping Knight Scans", "https://skscans.com", "en")
+class KKJScans : Genkan("KKJ Scans", "https://kkjscans.co", "en")
 class KrakenScans : Genkan("Kraken Scans", "https://krakenscans.com", "en")
