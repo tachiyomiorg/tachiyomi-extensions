7bcee1eef6704ddf15d947a4b853109d99e71d0b
arkon <eugcheung94@gmail.com>
2020-12-11 09:26:53 -0500

    Add example of how to only load a single extension in Gradle project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ project(":duktape-stub").projectDir = File("lib/duktape-stub")
 include(":lib-dataimage")
 project(":lib-dataimage").projectDir = File("lib/dataimage")
 
+// Loads all extensions
 File(rootDir, "src").eachDir { dir ->
     dir.eachDir { subdir ->
         val name = ":${dir.name}-${subdir.name}"
@@ -17,6 +18,12 @@ File(rootDir, "src").eachDir { dir ->
         project(name).projectDir = File("src/${dir.name}/${subdir.name}")
     }
 }
+
+// Use this to load a single extension during development
+// val lang = "all"
+// val name = "mmrcms"
+// include(":${lang}-${name}")
+// project(":${lang}-${name}").projectDir = File("src/${lang}/${name}")
 
 inline fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
