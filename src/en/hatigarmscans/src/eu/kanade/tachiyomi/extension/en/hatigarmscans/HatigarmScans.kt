bcc598d70f617a3ff3183a89d523d7600fd68c4d
Aria Moradi <aria.moradi007@gmail.com>
2020-12-16 23:39:59 +0330

    remove leftover generated files

--- src/en/hatigarmscans/src/eu/kanade/tachiyomi/extension/en/hatigarmscans/HatigarmScans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.hatigarmscans
-
-import eu.kanade.tachiyomi.lib.themesources.genkanoriginal.GenkanOriginal
-
-class HatigarmScans : GenkanOriginal("Hatigarm Scans", "https://hatigarmscanz.net", "en")
