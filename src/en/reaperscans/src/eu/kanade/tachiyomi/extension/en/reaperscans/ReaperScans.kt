e19839e42576c36466a0dcf36f6e3fb644225c67
Aria Moradi <aria.moradi007@gmail.com>
2020-12-16 23:38:09 +0330

    remove leftover generated files

--- src/en/reaperscans/src/eu/kanade/tachiyomi/extension/en/reaperscans/ReaperScans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.reaperscans
-
-import eu.kanade.tachiyomi.lib.themesources.genkanoriginal.GenkanOriginal
-
-class ReaperScans : GenkanOriginal("Reaper Scans", "https://reaperscans.com", "en")
--- src/en/secretscans/src/eu/kanade/tachiyomi/extension/en/secretscans/SecretScans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.secretscans
-
-import eu.kanade.tachiyomi.lib.themesources.genkanoriginal.GenkanOriginal
-
-class SecretScans : GenkanOriginal("SecretScans", "https://secretscans.co", "en")
