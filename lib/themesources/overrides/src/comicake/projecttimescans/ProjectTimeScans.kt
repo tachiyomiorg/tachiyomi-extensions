5d32b8daa06d3db82c8156da50ef509e4f52b70a
Aria Moradi <aria.moradi007@gmail.com>
2020-12-19 03:20:04 +0330

    comicake splited

---
+++ lib/themesources/overrides/src/comicake/projecttimescans/ProjectTimeScans.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.projecttimescans
+
+import eu.kanade.tachiyomi.lib.themesources.comicake.ComiCake
+
+class ProjectTimeScans : ComiCake("ProjectTime Scans", "https://read.ptscans.com", "en", "/")
