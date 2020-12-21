d60769595a4927d96fa27e34e6868fc03a124d04
Aria Moradi <aria.moradi007@gmail.com>
2020-12-21 21:18:07 +0330

    comments

--- multisrc/build.gradle.kts
+++ multisrc/build.gradle.kts
@@ -31,14 +31,14 @@ tasks.register("runAllGenerators") {
     doLast {
         var classPath = ""
         classPath += configurations.androidApis.get().asFileTree.first().absolutePath + ":" // android.jar path
-        classPath += "$projectDir/build/intermediates/aar_main_jar/debug/classes.jar:"
+        classPath += "$projectDir/build/intermediates/aar_main_jar/debug/classes.jar:" // jar made from this module
 
-        configurations.debugCompileOnly.get().asFileTree.forEach { classPath = "$classPath:$it" }
+        configurations.debugCompileOnly.get().asFileTree.forEach { classPath = "$classPath:$it" } // every dependency we have
 
-        val javaPath = System.getProperty("java.home") + "/bin/java"
+        val javaPath = System.getProperty("java.home") + "/bin/java" // path of java
 
-        val mainClass = "eu.kanade.tachiyomi.multisrc.GeneratorMain"
+        val mainClass = "eu.kanade.tachiyomi.multisrc.GeneratorMain" // Main class we want to execute
 
-        Runtime.getRuntime().exec("$javaPath -classpath $classPath $mainClass")
+        Runtime.getRuntime().exec("$javaPath -classpath $classPath $mainClass") // put it all together
     }
 }
