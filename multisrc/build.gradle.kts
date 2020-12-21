9fddb4e935be142fd9b114fc2149d28d44290dc7
Aria Moradi <aria.moradi007@gmail.com>
2020-12-21 18:32:42 +0330

    cleanup even more

--- multisrc/build.gradle.kts
+++ multisrc/build.gradle.kts
@@ -29,11 +29,9 @@ apply("$rootDir/common-dependencies.gradle")
 
 tasks.register("runAllGenerators") {
     doLast {
-        val projectRoot = rootProject.projectDir
-
         var classPath = ""
         classPath += configurations.androidApis.get().asFileTree.first().absolutePath + ":" // android.jar path
-        classPath += "$projectRoot/multisrc/build/intermediates/aar_main_jar/debug/classes.jar:"
+        classPath += "$projectDir/build/intermediates/aar_main_jar/debug/classes.jar:"
 
         configurations.debugCompileOnly.get().asFileTree.forEach { classPath = "$classPath:$it" }
 
@@ -44,3 +42,41 @@ tasks.register("runAllGenerators") {
         Runtime.getRuntime().exec("$javaPath -classpath $classPath $mainClass")
     }
 }
+tasks.register("runAllGenerators2") {
+    doLast {
+//        project.android.sourceSets.forEach {
+//            println(it)
+//        }
+//        android.sourceSets.forEach {
+//            println(it)
+//        }
+//
+//        println(project.android.bootClasspath)
+//        println(android.sourceSets.javaClass.name)
+//        android.sourceSets.forEach { println(it.javaClass.name) }
+//        configurations.debugCompileOnly.forEach {print(it)}
+//        android.bootClasspath
+    }
+//    configurations.runtimeOnly.get().allDependencies.forEach{println(it)}
+//    configurations.forEach{
+//        try {
+//            if (it.isCanBeResolved) {
+//                println(it)
+////        it.asFileTree
+//                it.asFileTree.forEach { println(it) }
+//                println()
+//                println()
+//            }
+//        }
+//        catch(e: Exception){}
+//    }
+//    configurations.debugCompileOnly.get().asFileTree.forEach { println(it) }
+//    configurations.androidApis.get().asFileTree.first().let { println(it) }
+    println(projectDir)
+
+}
+//
+//task("execute", JavaExec::class) {
+//    main = "eu.kanade.tachiyomi.multisrc.GeneratorMain"
+//    classpath = files("$buildDir/build/tmp/kotlin-classes/debug") + configurations.debugCompileOnly.get().asFileTree + configurations.androidApis.get().asFileTree
+//}
