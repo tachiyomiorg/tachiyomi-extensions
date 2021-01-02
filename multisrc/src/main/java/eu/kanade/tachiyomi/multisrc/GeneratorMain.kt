ec90145a7967037e350ce6c2aec4b1817a72a56d
Aria Moradi <aria.moradi007@gmail.com>
2021-01-02 12:54:22 +0330

    refactor

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/GeneratorMain.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/GeneratorMain.kt
@@ -15,20 +15,19 @@ fun main(args: Array<String>) {
         File(sourcesDir, it).isDirectory
     }
 
+    // find all theme packages
     directories.forEach { themeSource ->
-        // find all XxxGenerator.kt fgailes
-        val generatorClasses = File("$sourcesDirPath/$themeSource").list()!!.filter {
-            it.endsWith("Generator.kt")
-        }
-
-        // invoke main methods
-        generatorClasses.forEach {
-            val generatorClassPath = "eu/kanade/tachiyomi/multisrc/$themeSource/$it".replace("/", ".").substringBefore(".kt")
-            Class.forName(generatorClassPath).methods.forEach {
-                if (it.name == "main")
-                    it.invoke(null, emptyArray<String>())
+        // find all XxxGenerator.kt files and invoke main from them
+        File("$sourcesDirPath/$themeSource").list()!!
+            .filter {
+                it.endsWith("Generator.kt")
+            }.map {// find java class and extract method lists
+                Class.forName("eu/kanade/tachiyomi/multisrc/$themeSource/$it".replace("/", ".").substringBefore(".kt")).methods.asList()
             }
-        }
+            .flatten()
+            .filter { it.name == "main" }
+            .forEach { it.invoke(null, emptyArray<String>()) }
     }
 }
+
 
