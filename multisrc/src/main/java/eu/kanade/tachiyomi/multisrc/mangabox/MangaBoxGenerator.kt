14c2d00b9d0db0bac87d431e0c9bf5d5fc03ef66
Aria Moradi <aria.moradi007@gmail.com>
2021-02-07 05:24:38 +0330

    fix build

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangabox/MangaBoxGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangabox/MangaBoxGenerator.kt
@@ -1,10 +1,8 @@
 package eu.kanade.tachiyomi.multisrc.mangabox
 
 import eu.kanade.tachiyomi.multisrc.ThemeSourceGenerator
-import eu.kanade.tachiyomi.multisrc.ThemeSourceGenerator.Companion.SingleLangThemeSourceData
-import eu.kanade.tachiyomi.multisrc.ThemeSourceGenerator.Companion.MultiLangThemeSourceData
-import java.text.SimpleDateFormat
-import java.util.*
+import eu.kanade.tachiyomi.multisrc.ThemeSourceGenerator.Companion.ThemeSourceData.SingleLang
+
 
 class MangaBoxGenerator : ThemeSourceGenerator {
 
@@ -15,11 +13,11 @@ class MangaBoxGenerator : ThemeSourceGenerator {
     override val baseVersionCode: Int = 1
 
     override val sources = listOf(
-        SingleLangThemeSourceData("Mangakakalot", "https://mangakakalot.com", "en"),
-        SingleLangThemeSourceData("Manganelo", "https://manganelo.com", "en"),
-        SingleLangThemeSourceData("Mangabat", "https://mangabat.com", "en"),
-        SingleLangThemeSourceData("Mangakakalots (unoriginal)", "https://mangakakalots.com", "en", className = "Mangakakalots", pkgName = "mangakakalots"),
-        SingleLangThemeSourceData("Mangairo", "https://m.mangairo.com", "en"),
+        SingleLang("Mangakakalot", "https://mangakakalot.com", "en"),
+        SingleLang("Manganelo", "https://manganelo.com", "en"),
+        SingleLang("Mangabat", "https://mangabat.com", "en"),
+        SingleLang("Mangakakalots (unoriginal)", "https://mangakakalots.com", "en", className = "Mangakakalots", pkgName = "mangakakalots"),
+        SingleLang("Mangairo", "https://m.mangairo.com", "en"),
     )
 
     companion object {
