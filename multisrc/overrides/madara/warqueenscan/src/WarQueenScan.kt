3aac5cb266885ea82b462d9f3fec48fb1ecf6500
Riztard <riyanluqman@gmail.com>
2021-02-09 17:02:26 +0700

    N-Z

---
+++ multisrc/overrides/madara/warqueenscan/src/WarQueenScan.kt
@@ -0,0 +1,8 @@
+package eu.kanade.tachiyomi.extension.pt.warqueenscan
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.*
+
+class WarQueenScan : Madara("War Queen Scan", "https://wqscan.com", "pt-BR", SimpleDateFormat("dd/MM/yyyy", Locale.US))
+
