ded061bbe73f48e19bac6070cb0e5b64daf1c93e
Alessandro Jean <alessandrojean@gmail.com>
2021-02-11 16:41:29 -0300

    Temporary fix Cloudflare bypassing in Leitor.net.

--- src/pt/mangasproject/src/eu/kanade/tachiyomi/extension/pt/mangasproject/MangasProjectFactory.kt
+++ src/pt/mangasproject/src/eu/kanade/tachiyomi/extension/pt/mangasproject/MangasProjectFactory.kt
@@ -6,6 +6,7 @@ import eu.kanade.tachiyomi.source.SourceFactory
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
+import eu.kanade.tachiyomi.source.model.SChapter
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
@@ -24,6 +25,28 @@ class LeitorNet : MangasProject("Leitor.net", "https://leitor.net") {
     // became a different website, but they still use the same structure.
     // Existing mangas and other titles in the library still work.
     override val id: Long = 2225174659569980836
+
+    /**
+     * Temporary fix to bypass Cloudflare.
+     */
+    override fun pageListRequest(chapter: SChapter): Request {
+        val newHeaders = super.pageListRequest(chapter).headers().newBuilder()
+            .set("Referer", "https://mangalivre.net/home")
+            .build()
+
+        val newChapterUrl = chapter.url
+            .replace("/manga/", "/ler/")
+            .replace("/(\\d+)/capitulo-".toRegex(), "/online/$1/capitulo-")
+
+        return GET("https://mangalivre.net$newChapterUrl", newHeaders)
+    }
+
+    override fun getChapterUrl(response: Response): String {
+        return super.getChapterUrl(response)
+            .replace("https://mangalivre.net", baseUrl)
+            .replace("/ler/", "/manga/")
+            .replace("/online/", "/")
+    }
 }
 
 class MangaLivre : MangasProject("Mangá Livre", "https://mangalivre.net") {
