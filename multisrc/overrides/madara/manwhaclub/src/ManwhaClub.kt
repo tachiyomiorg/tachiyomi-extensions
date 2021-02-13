efd13b124851da051a2ec046268de748e74e5c90
Aria Moradi <aria.moradi007@gmail.com>
2021-02-13 18:00:13 +0330

    fix code

--- multisrc/overrides/madara/manwhaclub/src/ManwhaClub.kt
+++ multisrc/overrides/madara/manwhaclub/src/ManwhaClub.kt
@@ -2,6 +2,6 @@ package eu.kanade.tachiyomi.extension.en.manwhaclub
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class ManwhaClub : Madara("Manwha Club", "https://manhwa.club", "en") {
+class ManwhaClub : Madara("Manhwa.club", "https://manhwa.club", "en") {
     override val id = 6951399865568003192
 }
