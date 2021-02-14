5e471cb8e3f18308d5d80434ecc6e97d68b86268
Riztard <riyanluqman@gmail.com>
2021-02-14 23:29:07 +0700

    nsfw2

--- multisrc/overrides/foolslide/baixarhentai/src/BaixarHentai.kt
+++ multisrc/overrides/foolslide/baixarhentai/src/BaixarHentai.kt
@@ -1,9 +1,11 @@
 package eu.kanade.tachiyomi.extension.pt.baixarhentai
 
 import eu.kanade.tachiyomi.multisrc.foolslide.FoolSlide
+import eu.kanade.tachiyomi.annotations.Nsfw
 import eu.kanade.tachiyomi.source.model.SManga
 import org.jsoup.nodes.Document
 
+@Nsfw
 class BaixarHentai : FoolSlide("Baixar Hentai", "https://leitura.baixarhentai.net", "pt-BR") {
     // Hardcode the id because the language wasn't specific.
     override val id: Long = 8908032188831949972
