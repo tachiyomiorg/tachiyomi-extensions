660284c2f8e29b29879b98c3ea8a3dbfe44fc172
Riztard <riyanluqman@gmail.com>
2021-03-02 21:43:58 +0700

    remove import

--- multisrc/overrides/madara/manhuaes/src/ManhuaES.kt
+++ multisrc/overrides/madara/manhuaes/src/ManhuaES.kt
@@ -1,12 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.manhuaes
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.FilterList
-import eu.kanade.tachiyomi.source.model.SManga
-import okhttp3.Request
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
