d1f3408643dfafd4bfd5f513ee4c8e2746113059
OncePunchedMan <myniceaddress@outlook.com>
2021-03-10 18:21:15 +0100

    remove version override

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/genkan/GenkanOriginalGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/genkan/GenkanOriginalGenerator.kt
@@ -14,7 +14,7 @@ class GenkanOriginalGenerator : ThemeSourceGenerator {
     override val sources = listOf(
         SingleLang("Reaper Scans", "https://reaperscans.com", "en"),
         SingleLang("Hatigarm Scans", "https://hatigarmscanz.net", "en", overrideVersionCode = 1),
-        SingleLang("LynxScans", "https://lynxscans.com", "en", overrideVersionCode = 1),
+        SingleLang("LynxScans", "https://lynxscans.com", "en"),
     )
 
     companion object {
