e9790f41387a287781be35439e89461ce9b6765b
Johannes Joens <34899572+jopejoe1@users.noreply.github.com>
2021-04-01 03:02:48 +1300

    Update pkg and class names

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangamainac/MangaMainacGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangamainac/MangaMainacGenerator.kt
@@ -12,26 +12,26 @@ class MangaMainacGenerator : ThemeSourceGenerator {
     override val baseVersionCode: Int = 1
 
     override val sources = listOf(
-        SingleLang("Read Boku No Hero Academia Manga Online", "https://w23.readheroacademia.com/", "en", className = "ReadBokuNoHeroAcademiaMangaOnline", pkgName = "readbokunoheroacademiamangaonline"),
-        SingleLang("Read One Punch Man Manga Online", "https://w17.readonepunchman.net/", "en", className = "ReadOnePunchManMangaOnline", pkgName = "readonepunchmanmangaonline"),
-        SingleLang("Read One Webcomic Manga Online", "https://w1.onewebcomic.net/", "en", className = "ReadOneWebcomicMangaOnline", pkgName = "readonewebcomicmangaonline"),
-        SingleLang("Read Solo Leveling", "https://w3.sololeveling.net/", "en", className = "ReadSoloLeveling", pkgName = "readsololeveling"),
-        SingleLang("Read Jojolion", "https://readjojolion.com/", "en", className = "ReadJojolion", pkgName = "readjojolion"),
-        SingleLang("Hajime no Ippo Manga", "https://readhajimenoippo.com/", "en", className = "HajimeNoIppoManga", pkgName = "hajimenoippomanga"),
-        SingleLang("Read Berserk Manga Online", "https://berserkmanga.net/", "en", className = "ReadBerserkMangaOnline", pkgName = "readberserkmangaonline"),
+        SingleLang("Read Boku No Hero Academia Manga Online", "https://w23.readheroacademia.com/", "en"),
+        SingleLang("Read One Punch Man Manga Online", "https://w17.readonepunchman.net/", "en"),
+        SingleLang("Read One Webcomic Manga Online", "https://w1.onewebcomic.net/", "en"),
+        SingleLang("Read Solo Leveling", "https://w3.sololeveling.net/", "en"),
+        SingleLang("Read Jojolion", "https://readjojolion.com/", "en"),
+        SingleLang("Hajime no Ippo Manga", "https://readhajimenoippo.com/", "en"),
+        SingleLang("Read Berserk Manga Online", "https://berserkmanga.net/", "en"),
         SingleLang("Read Kaguya-sama: Love is War", "https://kaguyasama.net/", "en", className = "ReadKaguyaSamaLoveIsWar", pkgName = "readkaguyasamaloveiswar"),
-        SingleLang("Read Domestic Girlfriend Manga", "https://domesticgirlfriend.net/", "en", className = "ReadDomesticGirlfriendManga", pkgName = "readdomesticgirlfriendmanga"),
-        SingleLang("Read Black Clover Manga", "https://w1.blackclovermanga2.com/", "en", className = "ReadBlackCloverManga", pkgName = "readblackclovermanga"),
-        SingleLang("TCB Scans", "https://onepiecechapters.com/", "en", className = "TCBScans", pkgName = "tcbscans", overrideVersionCode = 2),
-        SingleLang("Read Shingeki no Kyojin Manga", "https://readshingekinokyojin.com/", "en", className = "ReadShingekiNoKyojinManga", pkgName = "readshingekinokyojinmanga"),
-        SingleLang("Read Nanatsu no Taizai Manga", "https://w1.readnanatsutaizai.net/", "en", className = "ReadNanatsuNoTaizaiManga", pkgName = "readnanatsunotaizaimanga"),
-        SingleLang("Read Rent a Girlfriend Manga", "https://kanojo-okarishimasu.com/", "en", className = "ReadRentAGirlfriendManga", pkgName = "readrentagirlfriendmanga"),
+        SingleLang("Read Domestic Girlfriend Manga", "https://domesticgirlfriend.net/", "en""),
+        SingleLang("Read Black Clover Manga", "https://w1.blackclovermanga2.com/", "en"),
+        SingleLang("TCB Scans", "https://onepiecechapters.com/", "en", overrideVersionCode = 2),
+        SingleLang("Read Shingeki no Kyojin Manga", "https://readshingekinokyojin.com/", "en"),
+        SingleLang("Read Nanatsu no Taizai Manga", "https://w1.readnanatsutaizai.net/", "en"),
+        SingleLang("Read Rent a Girlfriend Manga", "https://kanojo-okarishimasu.com/", "en"),
         //Sites that are currently down from my end, should be rechecked by some one else at some point
         //
-        //SingleLang("", "https://5-toubunnohanayome.net/", "en", className = "", pkgName = ""), //Down at time of creating this generator
-        //SingleLang("", "http://beastars.net/", "en", className = "", pkgName = ""), //Down at time of creating this generator
-        //SingleLang("", "https://neverlandmanga.net/", "en", className = "", pkgName = ""), //Down at time of creating this generator
-        //SingleLang("", "https://ww1.readhunterxhunter.net/", "en", className = "", pkgName = ""), //Down at time of creating this generator
+        //SingleLang("", "https://5-toubunnohanayome.net/", "en"), //Down at time of creating this generator
+        //SingleLang("", "http://beastars.net/", "en"), //Down at time of creating this generator
+        //SingleLang("", "https://neverlandmanga.net/", "en"), //Down at time of creating this generator
+        //SingleLang("", "https://ww1.readhunterxhunter.net/", "en"), //Down at time of creating this generator
     )
 
     companion object {
