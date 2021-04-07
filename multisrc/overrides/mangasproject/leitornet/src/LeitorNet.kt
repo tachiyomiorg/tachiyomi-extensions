fb7ffc8ab81a8b6410eabad22ed6d11c0c6d4552
Johannes Joens <34899572+jopejoe1@users.noreply.github.com>
2021-04-08 00:17:13 +1200

    Update LeitorNet.kt

--- multisrc/overrides/mangasproject/leitornet/src/LeitorNet.kt
+++ multisrc/overrides/mangasproject/leitornet/src/LeitorNet.kt
@@ -6,6 +6,11 @@ import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SChapter
 import okhttp3.Request
 import okhttp3.Response
+import okhttp3.OkHttpClient
+import okhttp3.FormBody
+import okhttp3.Headers
+import okhttp3.HttpUrl
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
 
 class LeitorNet : MangasProject("Leitor.net", "https://leitor.net", "pt-br") {
 
@@ -14,6 +19,13 @@ class LeitorNet : MangasProject("Leitor.net", "https://leitor.net", "pt-br") {
     // became a different website, but they still use the same structure.
     // Existing mangas and other titles in the library still work.
     override val id: Long = 2225174659569980836
+    
+    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
+        .addInterceptor(RateLimitInterceptor(5, 1, TimeUnit.SECONDS))
+        .connectTimeout(1, TimeUnit.MINUTES)
+        .readTimeout(1, TimeUnit.MINUTES)
+        .writeTimeout(1, TimeUnit.MINUTES)
+        .build()
 
     /**
      * Temporary fix to bypass Cloudflare.
