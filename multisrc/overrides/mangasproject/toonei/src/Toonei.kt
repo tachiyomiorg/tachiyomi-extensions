42d730c291c2188038218328da82aa84c9ebc362
Johannes Joens <34899572+jopejoe1@users.noreply.github.com>
2021-04-08 04:00:24 +1200

    Update Toonei.kt

--- multisrc/overrides/mangasproject/toonei/src/Toonei.kt
+++ multisrc/overrides/mangasproject/toonei/src/Toonei.kt
@@ -9,17 +9,9 @@ import okhttp3.FormBody
 import okhttp3.Headers
 import okhttp3.HttpUrl
 import java.util.concurrent.TimeUnit
-import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
 
 class Toonei : MangasProject("Toonei", "https://toonei.com", "pt-br") {
     
-    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
-        .addInterceptor(RateLimitInterceptor(5, 1, TimeUnit.SECONDS))
-        .connectTimeout(1, TimeUnit.MINUTES)
-        .readTimeout(1, TimeUnit.MINUTES)
-        .writeTimeout(1, TimeUnit.MINUTES)
-        .build()
-
     override fun getReaderToken(document: Document): String? {
         return document.select("script:containsData(window.PAGES_KEY)").firstOrNull()
             ?.data()
