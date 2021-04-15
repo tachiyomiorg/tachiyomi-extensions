dbc306ec919446867029462eaf5f4d19521c85b6
Johannes Joens <34899572+jopejoe1@users.noreply.github.com>
2021-04-16 07:25:31 +1200

    Added Overides

---
+++ multisrc/overrides/mmrcms/mangasyuri/src/MangasYuri.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.pt.mangasyuri
+
+import eu.kanade.tachiyomi.multisrc.mmrcms.MMRCMS
+
+class MangasYuri : MMRCMS("Mangás Yuri", "https://mangasyuri.net", "pt-BR"){
+    override val id: Long = 6456162511058446409
+}
