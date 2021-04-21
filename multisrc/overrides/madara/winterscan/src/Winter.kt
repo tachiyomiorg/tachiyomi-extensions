2983ebc4397ba3ec07c7030bea964dfea1c0ad45
Matheus William <matheuswr89@outlook.com>
2021-04-20 21:28:42 -0300

    Update Winter.kt

--- multisrc/overrides/madara/winterscan/src/Winter.kt
+++ multisrc/overrides/madara/winterscan/src/Winter.kt
@@ -4,4 +4,4 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class ArgosScan : Madara("Winter", "https://winterscan.com.br", "pt-BR", SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("pt", "BR")))
+class Winter : Madara("Winter", "https://winterscan.com.br", "pt-BR", SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("pt", "BR")))
