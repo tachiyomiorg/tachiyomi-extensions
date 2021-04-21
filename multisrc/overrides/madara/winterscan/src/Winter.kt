eb5029750c2d0fe28f866842d0a8f83be4464009
Matheus William <matheuswr89@outlook.com>
2021-04-20 21:33:16 -0300

    Update and rename Winter.kt to WinterScan.kt

--- multisrc/overrides/madara/winterscan/src/Winter.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.pt.argosscan
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class Winter : Madara("Winter", "https://winterscan.com.br", "pt-BR", SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("pt", "BR")))
