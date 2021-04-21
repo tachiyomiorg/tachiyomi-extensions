56562ffee92e4b29c21a8a3b8d5622dadaad8eb0
Matheus William <matheuswr89@outlook.com>
2021-04-20 21:31:48 -0300

    Update LittleMonsterScan.kt

--- multisrc/overrides/madara/littlemonsterscan/src/LittleMonsterScan.kt
+++ multisrc/overrides/madara/littlemonsterscan/src/LittleMonsterScan.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.pt.argosscan
+package eu.kanade.tachiyomi.extension.pt.littlemonsterscan
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
