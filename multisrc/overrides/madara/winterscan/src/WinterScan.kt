c37563f2e9bcff02f0fc8646ca0ba82806b9b597
Matheus William <matheuswr89@outlook.com>
2021-04-20 23:43:13 -0300

    Update url 5

--- multisrc/overrides/madara/winterscan/src/WinterScan.kt
+++ multisrc/overrides/madara/winterscan/src/WinterScan.kt
@@ -4,5 +4,5 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class WinterScan : Madara("Winter Scan", "https://winterscan.com.br/", 
+class WinterScan : Madara("Winter Scan", "https://winterscan.com.br", 
         "pt-BR", SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("pt", "BR")))
