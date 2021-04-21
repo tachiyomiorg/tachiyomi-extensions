800b5d6d3be14be2bab150175727061817c4c005
Matheus William <matheuswr89@outlook.com>
2021-04-20 23:43:58 -0300

    Update url 6

--- multisrc/overrides/madara/wonderland/src/Wonderland.kt
+++ multisrc/overrides/madara/wonderland/src/Wonderland.kt
@@ -4,5 +4,5 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class Wonderland : Madara("Wonderland", "https://landwebtoons.site/", 
+class Wonderland : Madara("Wonderland", "https://landwebtoons.site", 
       "pt-BR", SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale("pt", "BR")))
