404dd633eb62befd9e191a979d350b935cb7e765
Alessandro Jean <alessandrojean@gmail.com>
2021-05-13 14:33:54 -0300

    Add open modifier to imageFromElement again.

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/Madara.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/Madara.kt
@@ -398,7 +398,7 @@ abstract class Madara(
         return this.contains(updatingRegex).not()
     }
 
-    protected fun imageFromElement(element: Element): String? {
+    protected open fun imageFromElement(element: Element): String? {
         return when {
             element.hasAttr("data-src") -> element.attr("abs:data-src")
             element.hasAttr("data-lazy-src") -> element.attr("abs:data-lazy-src")
