44f6524418ea2e51b298f66cab15432e543ec49b
NMBLM <nunomblm.1997@gmail.com>
2021-05-27 17:03:58 +0100

    Fixed Status to show correctly

--- src/en/mangarawclub/src/eu/kanade/tachiyomi/extension/en/mangarawclub/MangaRawClub.kt
+++ src/en/mangarawclub/src/eu/kanade/tachiyomi/extension/en/mangarawclub/MangaRawClub.kt
@@ -84,20 +84,18 @@ class MangaRawClub : ParsedHttpSource() {
             genres.add(genre)
         }
         manga.genre = genres.joinToString(", ")
-        manga.status = parseStatus(document.select("h5:contains(Status) + div").text())
+        val status = when {
+            document.select("div.header-stats").select("strong.completed").first() != null -> SManga.COMPLETED
+            document.select("div.header-stats").select("strong.ongoing").first() != null -> SManga.ONGOING
+            else -> SManga.UNKNOWN
+        }
+        manga.status = status
         manga.description = document.getElementsByClass("description").first().text()
         manga.description = document.select("div.summary > div.content").first().text()
         val coverElement = document.getElementsByClass("cover")
         manga.thumbnail_url = baseUrl + coverElement.select("img").attr("data-src")
 
         return manga
-    }
-
-    private fun parseStatus(element: String): Int = when {
-
-        element.toLowerCase(Locale.getDefault()).contains("publishing") -> SManga.ONGOING
-        element.toLowerCase(Locale.getDefault()).contains("finished") -> SManga.COMPLETED
-        else -> SManga.UNKNOWN
     }
 
     override fun chapterListSelector() = "ul.chapter-list > li"
