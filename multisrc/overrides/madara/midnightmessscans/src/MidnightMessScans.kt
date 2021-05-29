1a9fc78d1fde91ed256852e91edc1b8667e04d74
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-05-30 01:44:05 +0800

    fix MMS thumbnail

---
+++ multisrc/overrides/madara/midnightmessscans/src/MidnightMessScans.kt
@@ -0,0 +1,22 @@
+package eu.kanade.tachiyomi.extension.en.midnightmessscans
+
+import eu.kanade.tachiyomi.annotations.Nsfw
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import okhttp3.OkHttpClient
+
+@Nsfw
+class MidnessMessScans : Madara("Midnight Mess Scans", "https://midnightmess.org, "en") {
+    override val client: OkHttpClient = super.client.newBuilder()
+        .addInterceptor { chain ->
+            val originalRequest = chain.request()
+            chain.proceed(originalRequest).let { response ->
+                if (response.code == 403) {
+                    response.close()
+                    chain.proceed(originalRequest.newBuilder().removeHeader("Referer").addHeader("Referer", "https://midnightmess.org").build())
+                } else {
+                    response
+                }
+            }
+        }
+        .build()
+}
