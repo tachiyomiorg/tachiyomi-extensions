657994cf1cabcff96b21c5abc51f78c218c01a66
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-05-30 16:15:20 +0800

    Create MangaKomi.kt

---
+++ multisrc/overrides/madara/mangakomi/src/MangaKomi.kt
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi.extension.en.mangakomi
+
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+class MangaKomi : Madara("Manga Komi", "https://mangakomi.com", "en") {
+    override val client: OkHttpClient = super.client.newBuilder()
+        .addInterceptor(RateLimitInterceptor(1, 1, TimeUnit.SECONDS))
+        .build()
+}
