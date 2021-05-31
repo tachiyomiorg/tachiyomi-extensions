5814f7cea1f53c640211e753694e5eda4a725323
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-05-31 15:21:12 +0800

    Create Manycomic.kt

---
+++ multisrc/overrides/madara/manycomic/src/Manycomic.kt
@@ -0,0 +1,24 @@
+package eu.kanade.tachiyomi.extension.en.manycomic
+
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
+import eu.kanade.tachiyomi.annotations.Nsfw
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+@Nsfw
+class Manycomic : Madara(
+    "Manycomic",
+    "https://manycomic.com",
+    "en"
+) {
+    private val rateLimitInterceptor = RateLimitInterceptor(1)
+
+    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .addNetworkInterceptor(rateLimitInterceptor)
+        .build()
+}
