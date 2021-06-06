37c822aeee8258604242d20cea8e41214fe2b87d
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-06-06 18:52:44 +0800

    pkgName

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -168,7 +168,7 @@ class MadaraGenerator : ThemeSourceGenerator {
             SingleLang("Manhwa.club", "https://manhwa.club", "en", className="ManwhaClub", overrideVersionCode = 2), // wrong class name for backward compatibility
             SingleLang("ManyToon", "https://manytoon.com", "en", overrideVersionCode = 1),
             SingleLang("ManyToonClub", "https://manytoon.club", "ko"),
-            SingleLang("ManyToon.me", "https://manytoon.me", "en", className = "ManyToonMe", isNsfw = true, overrideVersionCode = 2),
+            SingleLang("ManyToon.me", "https://manytoon.me", "en", pkgName = "manytoonme", className = "ManyToonMe", isNsfw = true, overrideVersionCode = 2),
             SingleLang("Mark Scans", "https://markscans.online", "pt-BR", overrideVersionCode = 1),
             SingleLang("MG Komik", "https://mgkomik.my.id", "id", overrideVersionCode = 1),
             SingleLang("Midnight Mess Scans", "https://midnightmess.org", "en", isNsfw = true, overrideVersionCode = 1),
