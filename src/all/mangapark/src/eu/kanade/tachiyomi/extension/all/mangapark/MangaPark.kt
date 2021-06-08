d7edf38fc6212a181bc39052cd679a3b8457e81c
FourTOne5 <59261191+FourTOne5@users.noreply.github.com>
2021-06-08 07:55:34 +0600

    Update MangaPark.kt

--- src/all/mangapark/src/eu/kanade/tachiyomi/extension/all/mangapark/MangaPark.kt
+++ src/all/mangapark/src/eu/kanade/tachiyomi/extension/all/mangapark/MangaPark.kt
@@ -219,8 +219,8 @@ open class MangaPark(
     }
     
     override fun chapterListParse(response: Response): List<SChapter> {
-        val resTojson = json.parseToJsonElement(response.toString()).jsonObject
-        val document = Jsoup.parse(resTojson["html"]!!.toString())
+        val resTojson = json.parseToJsonElement(response.body()!!.string()).jsonObject
+        val document = Jsoup.parse(resTojson["html"]!!.string())
         return document.select(chapterListSelector()).map { chapterFromElement(it) }
     }
     
