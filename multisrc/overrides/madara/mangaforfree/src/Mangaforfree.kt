4478f196e6a6ae95365f40b236624ca4d049fbf7
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-06-09 13:57:51 +0800

    add Raw to genre

--- multisrc/overrides/madara/mangaforfree/src/Mangaforfree.kt
+++ multisrc/overrides/madara/mangaforfree/src/Mangaforfree.kt
@@ -46,6 +46,7 @@ class Mangaforfree : Madara("Mangaforfree", "https://mangaforfree.net", "en") {
         Genre("Mystery", "mystery"),
         Genre("One shot", "one-shot"),
         Genre("Psychological", "psychological"),
+        Genre("Raw", "raw"),
         Genre("Romance", "romance"),
         Genre("School Life", "school-life"),
         Genre("Sci-fi", "sci-fi"),
