cc511da9ee4bff12bfe4852debb53fe9b956c2cc
Eugene <35057681+e-shl@users.noreply.github.com>
2021-06-13 17:14:32 +0500

    search and filters

--- src/ru/mangabook/src/eu/kanade/tachiyomi/extension/ru/mangabook/MangaBook.kt
+++ src/ru/mangabook/src/eu/kanade/tachiyomi/extension/ru/mangabook/MangaBook.kt
@@ -3,13 +3,17 @@ package eu.kanade.tachiyomi.extension.ru.mangabook
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Headers
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
@@ -53,21 +57,37 @@ class MangaBook : ParsedHttpSource() {
         val url = if (query.isNotBlank()) {
             "$baseUrl/dosearch?&query=$query"
         } else {
-            var ret = String()
+            val url = "$baseUrl/filterList?page=$page&ftype[]=0&status[]=0&sortBy=rate".toHttpUrlOrNull()!!.newBuilder()
             (if (filters.isEmpty()) getFilterList() else filters).forEach { filter ->
                 when (filter) {
-                    is GenreList -> {
-                        ret = "$baseUrl/genre/${filter.values[filter.state].id}/page/$page"
+                    is OrderBy -> {
+                        val ord = arrayOf("rate", "name", "views", "created_at")[filter.state]
+                        url.addQueryParameter("sortBy", "$ord")
+                    }
+                    is CategoryList -> filter.state.forEach { category ->
+                        if (category.state) {
+                            url.addQueryParameter("cat", category.id)
+                        }
+                    }
+                    is StatusList -> filter.state.forEach { status ->
+                        if (status.state) {
+                            url.addQueryParameter("status[]", status.id)
+                        }
+                    }
+                    is FormatList -> filter.state.forEach { forma ->
+                        if (forma.state) {
+                            url.addQueryParameter("ftype[]", forma.id)
+                        }
                     }
                 }
             }
-            ret
+            return GET(url.toString(), headers)
         }
         return GET(url, headers)
     }
 
     override fun searchMangaNextPageSelector() = popularMangaNextPageSelector()
-    override fun searchMangaSelector(): String = ".manga-list li:not(.vis )"
+    override fun searchMangaSelector(): String = popularMangaSelector()
     override fun searchMangaFromElement(element: Element): SManga {
         return SManga.create().apply {
             element.select(".flist.row a").first().let {
@@ -76,6 +96,17 @@ class MangaBook : ParsedHttpSource() {
             }
             thumbnail_url = element.select(".sposter img.img-responsive").attr("src")
         }
+    }
+
+    override fun searchMangaParse(response: Response): MangasPage {
+        if (!response.request.url.toString().contains("dosearch")) {
+            return popularMangaParse(response)
+        }
+        val document = response.asJsoup()
+        val mangas = document.select(".manga-list li:not(.vis )").map { element ->
+            searchMangaFromElement(element)
+        }
+        return MangasPage(mangas, false)
     }
 
     // Details
@@ -121,64 +152,108 @@ class MangaBook : ParsedHttpSource() {
     }
 
     override fun imageUrlParse(document: Document) = throw Exception("imageUrlParse Not Used")
+
     // Filters
-    private class Genre(name: String, val id: String) : Filter.CheckBox(name) {
-        override fun toString(): String {
-            return name
-        }
-    }
+    private class CheckFilter(name: String, val id: String) : Filter.CheckBox(name)
 
-    private class GenreList(genres: Array<Genre>) : Filter.Select<Genre>("Genres", genres, 0)
-
+    private class FormatList(formas: List<CheckFilter>) : Filter.Group<CheckFilter>("Тип", formas)
+    private class StatusList(statuses: List<CheckFilter>) : Filter.Group<CheckFilter>("Статус", statuses)
+    private class CategoryList(categories: List<CheckFilter>) : Filter.Group<CheckFilter>("Категории", categories)
     override fun getFilterList() = FilterList(
-        GenreList(getGenreList())
+        OrderBy(),
+        CategoryList(getCategoryList()),
+        StatusList(getStatusList()),
+        FormatList(getFormatList())
     )
 
-    private fun getGenreList() = arrayOf(
-        Genre("Все", "all"),
-        Genre("Боевик", "boevik"),
-        Genre("Боевые искусства", "boevye_iskusstva"),
-        Genre("Вампиры", "vampiry"),
-        Genre("Гарем", "garem"),
-        Genre("Гендерная интрига", "gendernaya_intriga"),
-        Genre("Героическое фэнтези", "geroicheskoe_fehntezi"),
-        Genre("Детектив", "detektiv"),
-        Genre("Дзёсэй", "dzyosehj"),
-        Genre("Додзинси", "dodzinsi"),
-        Genre("Драма", "drama"),
-        Genre("Игра", "igra"),
-        Genre("История", "istoriya"),
-        Genre("Меха", "mekha"),
-        Genre("Мистика", "mistika"),
-        Genre("Научная фантастика", "nauchnaya_fantastika"),
-        Genre("Повседневность", "povsednevnost"),
-        Genre("Постапокалиптика", "postapokaliptika"),
-        Genre("Приключения", "priklyucheniya"),
-        Genre("Психология", "psihologiya"),
-        Genre("Романтика", "romantika"),
-        Genre("Самурайский боевик", "samurajskij_boevik"),
-        Genre("Сверхъестественное", "sverhestestvennoe"),
-        Genre("Сёдзё", "syodzyo"),
-        Genre("Сёдзё-ай", "syodzyo-aj"),
-        Genre("Сёнэн", "syonen"),
-        Genre("Спорт", "sport"),
-        Genre("Сэйнэн", "sejnen"),
-        Genre("Трагедия", "tragediya"),
-        Genre("Триллер", "triller"),
-        Genre("Ужасы", "uzhasy"),
-        Genre("Фантастика", "fantastika"),
-        Genre("Фэнтези", "fentezi"),
-        Genre("Школа", "shkola"),
-        Genre("Этти", "etti"),
-        Genre("Юри", "yuri"),
-        Genre("Военный", "voennyj"),
-        Genre("Жосей", "zhosej"),
-        Genre("Магия", "magiya"),
-        Genre("Полиция", "policiya"),
-        Genre("Смена пола", "smena-pola"),
-        Genre("Супер сила", "super-sila"),
-        Genre("Эччи", "echchi"),
-        Genre("Яой", "yaoj"),
-        Genre("Сёнэн-ай", "syonen-aj")
+    private class OrderBy : Filter.Select<String>(
+        "Сортировка",
+        arrayOf("По рейтингу", "По алфавиту", "По популярности", "По дате выхода")
+    )
+    private fun getFormatList() = listOf(
+        CheckFilter("Манга", "1"),
+        CheckFilter("Манхва", "2"),
+        CheckFilter("Веб Манхва", "4"),
+        CheckFilter("Маньхуа", "3")
+    )
+
+    private fun getStatusList() = listOf(
+        CheckFilter("Сейчас издаётся", "1"),
+        CheckFilter("Анонсировано", "3"),
+        CheckFilter("Изданное", "2")
+    )
+
+    private fun getCategoryList() = listOf(
+        CheckFilter("16+", "16+"),
+        CheckFilter("Арт", "art"),
+        CheckFilter("Бара", "bara"),
+        CheckFilter("Боевик", "action"),
+        CheckFilter("Боевые искусства", "combatskill"),
+        CheckFilter("В цвете", "vcvete"),
+        CheckFilter("Вампиры", "vampaires"),
+        CheckFilter("Веб", "web"),
+        CheckFilter("Вестерн", "western"),
+        CheckFilter("Гарем", "harem"),
+        CheckFilter("Гендерная интрига", "genderintrigue"),
+        CheckFilter("Героическое фэнтези", "heroic_fantasy"),
+        CheckFilter("Детектив", "detective"),
+        CheckFilter("Дзёсэй", "josei"),
+        CheckFilter("Додзинси", "doujinshi"),
+        CheckFilter("Драма", "drama"),
+        CheckFilter("Ёнкома", "yonkoma"),
+        CheckFilter("Есси", "18+"),
+        CheckFilter("Зомби", "zombie"),
+        CheckFilter("Игра", "games"),
+        CheckFilter("Инцест", "incest"),
+        CheckFilter("Исекай", "isekai"),
+        CheckFilter("Искусство", "iskusstvo"),
+        CheckFilter("Исторический", "historical"),
+        CheckFilter("Киберпанк", "cyberpunk"),
+        CheckFilter("Кодомо", "kodomo"),
+        CheckFilter("Комедия", "comedy"),
+        CheckFilter("Культовое", "iconic"),
+        CheckFilter("литРПГ", "litrpg"),
+        CheckFilter("Любовь", "love"),
+        CheckFilter("Махо-сёдзё", "maho-shojo"),
+        CheckFilter("Меха", "robots"),
+        CheckFilter("Мистика", "mystery"),
+        CheckFilter("Мужская беременность", "male-pregnancy"),
+        CheckFilter("Музыка", "music"),
+        CheckFilter("Научная фантастика", "sciencefiction"),
+        CheckFilter("Новинки", "new"),
+        CheckFilter("Омегаверс", "omegavers"),
+        CheckFilter("Перерождение", "newlife"),
+        CheckFilter("Повседневность", "humdrum"),
+        CheckFilter("Постапокалиптика", "postapocalyptic"),
+        CheckFilter("Приключения", "adventure"),
+        CheckFilter("Психология", "psychology"),
+        CheckFilter("Романтика", "romance"),
+        CheckFilter("Самураи", "samurai"),
+        CheckFilter("Сборник", "compilation"),
+        CheckFilter("Сверхъестественное", "supernatural"),
+        CheckFilter("Сёдзё", "shojo"),
+        CheckFilter("Сёдзё-ай", "maho-shojo"),
+        CheckFilter("Сёнэн", "senen"),
+        CheckFilter("Сёнэн-ай", "shonen-ai"),
+        CheckFilter("Сетакон", "setakon"),
+        CheckFilter("Сингл", "singl"),
+        CheckFilter("Сказка", "fable"),
+        CheckFilter("Сорс", "bdsm"),
+        CheckFilter("Спорт", "sport"),
+        CheckFilter("Супергерои", "superheroes"),
+        CheckFilter("Сэйнэн", "seinen"),
+        CheckFilter("Танцы", "dancing"),
+        CheckFilter("Трагедия", "tragedy"),
+        CheckFilter("Триллер", "thriller"),
+        CheckFilter("Ужасы", "horror"),
+        CheckFilter("Фантастика", "fantastic"),
+        CheckFilter("Фурри", "furri"),
+        CheckFilter("Фэнтези", "fantasy"),
+        CheckFilter("Школа", "school"),
+        CheckFilter("Эротика", "erotica"),
+        CheckFilter("Этти", "etty"),
+        CheckFilter("Юмор", "humor"),
+        CheckFilter("Юри", "yuri"),
+        CheckFilter("Яой", "yaoi")
     )
 }
