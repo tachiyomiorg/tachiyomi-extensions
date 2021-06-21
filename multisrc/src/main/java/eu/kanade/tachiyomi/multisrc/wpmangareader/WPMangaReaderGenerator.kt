d2487f1ef1495f05b9c0c800d144f80517a4d52c
Narin <narin.tana@hotmail.com>
2021-06-21 18:42:26 +0700

    Fixed formatting

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpmangareader/WPMangaReaderGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpmangareader/WPMangaReaderGenerator.kt
@@ -32,7 +32,7 @@ class WPMangaReaderGenerator : ThemeSourceGenerator {
         SingleLang("GS Nation", "https://gs-nation.fr", "fr", overrideVersionCode = 1),
         SingleLang("YugenMangas", "https://yugenmangas.com", "es"),
         SingleLang("DragonTranslation", "https://dragontranslation.com", "es", isNsfw = true),
-        SingleLang("Patatescans", "https://patatescans.com", "fr", isNsfw=true),
+        SingleLang("Patatescans", "https://patatescans.com", "fr", isNsfw = true),
         SingleLang("Fusion Scanlation", "https://fusionscanlation.com", "es", isNsfw = true),
     )
 
