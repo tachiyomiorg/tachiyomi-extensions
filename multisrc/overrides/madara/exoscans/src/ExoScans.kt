bb82c5200b0131b54ca4800c1abb4d8b9c57a042
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-06-25 19:51:43 +0800

    Update ExoScans.kt

--- multisrc/overrides/madara/exoscans/src/ExoScans.kt
+++ multisrc/overrides/madara/exoscans/src/ExoScans.kt
@@ -6,16 +6,12 @@ import org.jsoup.nodes.Document
 
 class ExoScans : Madara("Exo Scans", "https://exoscans.club", "en") {
 
-    override val pageListParseSelector = "div.read-content img"
-
     override fun pageListParse(document: Document): List<Page> {
         return document.select(pageListParseSelector).mapIndexed { index, element ->
             Page(
                 index,
-                document.location(),
-                element?.let {
-                    it.absUrl(if (it.hasAttr("data-src")) "data-src" else "src")
-                }
+                "",
+                element.select("img.wp-manga-chapter-img").attr("src")
             )
         }
     }
