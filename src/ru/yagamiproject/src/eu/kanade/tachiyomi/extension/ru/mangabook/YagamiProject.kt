008695023a86a172f3888e107e3daede87a535eb
Eugene <35057681+e-shl@users.noreply.github.com>
2021-06-26 08:57:18 +0500

    [RU]YagamiProject universal chapter_number

--- src/ru/yagamiproject/src/eu/kanade/tachiyomi/extension/ru/mangabook/YagamiProject.kt
+++ src/ru/yagamiproject/src/eu/kanade/tachiyomi/extension/ru/mangabook/YagamiProject.kt
@@ -118,10 +118,9 @@ class YagamiProject : ParsedHttpSource() {
             chapter.attr("title").isNullOrBlank() -> chapter.text()
             else -> chapter.attr("title")
         }
-
+        val numout = name.substringBefore(":").substringAfterLast(" ").substringAfterLast("№")
         chapter_number = when {
-            name.contains("Глава") -> name.substringAfter("Глава ").substringBefore(":").toFloat()
-            name.contains("Акт") -> name.substringAfter("Акт").substringBefore(":").replace("№", "").toFloat()
+            numout.contains(Regex("^[0-9]+")) -> numout.toFloat()
             else -> 0F
         }
         setUrlWithoutDomain(chapter.attr("href"))
