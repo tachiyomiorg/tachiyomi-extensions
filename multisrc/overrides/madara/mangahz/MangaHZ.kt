80fdc26e122844d709424770a5e1b46a4e35a37e
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-01 16:45:54 +0800

    Delete MangaHZ.kt

--- multisrc/overrides/madara/mangahz/MangaHZ.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangahz
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.annotations.Nsfw
-
-@Nsfw
-class MangaHZ : Madara("MangaHZ", "https://mangahz.com", "en")
