9b4b5f819717e885f65f3ac73592decf3b5c0d65
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-01 16:44:21 +0800

    Create MangaForFreeCom.kt

---
+++ multisrc/overrides/madara/mangaforfreecom/src/MangaForFreeCom.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.mangaforfreecom
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.annotations.Nsfw
+
+@Nsfw
+class MangaForFreeCom : Madara("MangaForFree.com", "https://mangaforfree.com", "en")
