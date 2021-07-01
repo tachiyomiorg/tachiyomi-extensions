8a716ecb3d3209abef4082e6d2cda1059f480f3c
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-01 17:01:00 +0800

    Update MangaHZ.kt

--- multisrc/overrides/madara/mangahz/src/MangaHZ.kt
+++ multisrc/overrides/madara/mangahz/src/MangaHZ.kt
@@ -2,6 +2,8 @@ package eu.kanade.tachiyomi.extension.en.mangahz
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.annotations.Nsfw
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Nsfw
-class MangaHZ : Madara("MangaHZ", "https://mangahz.com", "en")
+class MangaHZ : Madara("MangaHZ", "https://mangahz.com", "en", dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.US))
