b9dd034e10157e6b393b60a5e46b9e4f7b1c131f
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-01 16:45:22 +0800

    Create MangaHZ.kt

---
+++ multisrc/overrides/madara/mangahz/MangaHZ.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.mangahz
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.annotations.Nsfw
+
+@Nsfw
+class MangaHZ : Madara("MangaHZ", "https://mangahz.com", "en")
