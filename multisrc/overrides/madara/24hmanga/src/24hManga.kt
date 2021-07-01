02fdab909e237d1a387ee12f9bc1740fa4cd0cbc
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-01 16:42:40 +0800

    Create 24hManga.kt

---
+++ multisrc/overrides/madara/24hmanga/src/24hManga.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.24hmanga
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.annotations.Nsfw
+
+@Nsfw
+class 24hManga : Madara("24hManga", "https://24hmanga.com", "en")
