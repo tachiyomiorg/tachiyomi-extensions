f3fb1b4ebce97cc2b59fa482d35189decea0e8cb
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-01 16:59:18 +0800

    Update 24hManga.kt

--- multisrc/overrides/madara/24hmanga/src/24hManga.kt
+++ multisrc/overrides/madara/24hmanga/src/24hManga.kt
@@ -2,6 +2,8 @@ package eu.kanade.tachiyomi.extension.en.24hmanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.annotations.Nsfw
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Nsfw
-class 24hManga : Madara("24hManga", "https://24hmanga.com", "en")
+class 24hManga : Madara("24hManga", "https://24hmanga.com", "en", dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.US))
