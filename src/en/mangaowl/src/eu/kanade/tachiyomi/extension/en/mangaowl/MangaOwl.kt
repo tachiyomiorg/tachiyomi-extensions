06524aac00bd9a2f702f7fd02d2379fb7d172da4
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-13 07:20:32 +0800

    Update MangaOwl.kt

--- src/en/mangaowl/src/eu/kanade/tachiyomi/extension/en/mangaowl/MangaOwl.kt
+++ src/en/mangaowl/src/eu/kanade/tachiyomi/extension/en/mangaowl/MangaOwl.kt
@@ -1,6 +1,11 @@
 package eu.kanade.tachiyomi.extension.en.mangaowl
 
 import eu.kanade.tachiyomi.network.GET
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
