bf2c56b41b47c23545ef617a890681c367a60f74
hntidat <hntidat@gmail.com>
2021-07-20 18:37:40 +0700

    update Header Nettruyen, Nhattruyen, TruyenChon

--- multisrc/overrides/wpcomics/truyenchon/src/TruyenChon.kt
+++ multisrc/overrides/wpcomics/truyenchon/src/TruyenChon.kt
@@ -10,6 +10,7 @@ import java.util.Locale
 
 class TruyenChon : WPComics("TruyenChon", "http://truyenchon.com", "vi", SimpleDateFormat("dd/MM/yy", Locale.getDefault()), null) {
     override val searchPath = "the-loai"
+    override fun headersBuilder(): Headers.Builder = Headers.Builder()
     override fun imageRequest(page: Page): Request = GET(page.imageUrl!!, headersBuilder().add("Referer", baseUrl).build())
     override fun getFilterList(): FilterList {
         return FilterList(
